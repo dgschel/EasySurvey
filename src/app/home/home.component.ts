8f6c4842fd721bac123637882c47372bcd399eae
dgschel <goesc_@hotmail.de>
2024-06-26 21:12:13 +0200

    consume new card component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BasicCardComponent } from '../shared/ui/basic-card/basic-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BasicCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
