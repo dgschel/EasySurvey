080aebe3feb467c7fde294d0e4b7eec8139cbfc3
dgschel <goesc_@hotmail.de>
2024-06-26 21:17:11 +0200

    add new input component

---
+++ src/app/shared/feature/form-control-input/form-control-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-control-input.component.html',
+  styleUrl: './form-control-input.component.scss'
+})
+export class FormControlInputComponent {
+
+}
