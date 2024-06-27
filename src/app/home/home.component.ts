477a9fc1018c6de580c85616b592aa1851d940f2
dgschel <goesc_@hotmail.de>
2024-06-27 17:41:47 +0200

    add basic usage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { BasicCardComponent } from '../shared/ui/basic-card/basic-card.component';
+import { FormControlInputComponent } from '../shared/feature/form-control-input/form-control-input.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BasicCardComponent],
+  imports: [ReactiveFormsModule, BasicCardComponent, FormControlInputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  form: FormGroup = new FormGroup({});
 
+  submit() {
+    console.log(this.form);
+  }
 }
