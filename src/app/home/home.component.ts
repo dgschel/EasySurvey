cc64ddb334bf2e9eb770a19f30e2059cd89f11f2
dgschel <goesc_@hotmail.de>
2024-06-27 21:14:46 +0200

    restructure html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BasicCardComponent } from '../shared/ui/basic-card/basic-card.component';
 import { FormControlInputComponent } from '../shared/feature/form-control-input/form-control-input.component';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule, BasicCardComponent, FormControlInputComponent],
+  imports: [ReactiveFormsModule, BasicCardComponent, FormControlInputComponent, JsonPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
