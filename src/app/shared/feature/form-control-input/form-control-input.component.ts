72cc571c450b1acea4c266e616ce29ba5188e2b2
dgschel <goesc_@hotmail.de>
2024-06-27 17:41:09 +0200

    receive parent and provide ng-content from viewproviders

--- src/app/shared/feature/form-control-input/form-control-input.component.ts
+++ src/app/shared/feature/form-control-input/form-control-input.component.ts
@@ -1,16 +1,29 @@
-import { Component, inject, input } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit, inject, input } from '@angular/core';
+import { ControlContainer, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-control-input.component.html',
-  styleUrl: './form-control-input.component.scss'
+  styleUrl: './form-control-input.component.scss',
+  viewProviders: [{ // viewProviders is used to provide the parent form group to the child component. Useful for content projection
+    provide: ControlContainer,
+    useFactory: () => inject(ControlContainer, { skipSelf: true })
+  }]
 })
-export class FormControlInputComponent {
+export class FormControlInputComponent implements OnInit {
   inputControlName = input.required<string>();
   inputPlaceholder = input<string>();
+  parentContainer = inject(ControlContainer);
 
   private nnfb = inject(FormBuilder);
+
+  get parentFormGroup() {
+    return this.parentContainer.control as FormGroup;
+  }
+
+  ngOnInit(): void {
+    this.parentFormGroup.addControl(this.inputControlName(), this.nnfb.control<string>(''));
+  }
 }
