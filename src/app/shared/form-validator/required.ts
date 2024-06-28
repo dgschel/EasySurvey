219ea8a428f0da6b6442fa34a407c4bc8126b350
dgschel <goesc_@hotmail.de>
2024-06-28 07:52:53 +0200

    rename custom form validation file

--- src/app/shared/form-validator/required.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
-
-export const customRequiredValidator = (): ValidatorFn => {
-  return (control: AbstractControl): ValidationErrors | null => {
-    // Use the built-in required validator
-    const requiredValidator = Validators.required(control);
-    return requiredValidator ? { required: 'message' } : null;
-  }
-}
---
+++ src/app/shared/form-validator/validators.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+
+export const customRequiredValidator = (message: string = "This field is required"): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    // Use the built-in required validator
+    const validate = Validators.required(control);
+    return validate ? { required: message } : null;
+  }
+}
+
+export const customMinLengthValidator = (minLength: number, message: string = `Min length of ${minLength} characters`): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    // Use the built-in minlength validator
+    const validate = Validators.minLength(minLength)(control);
+    return validate ? { required: message } : null;
+  }
+}
