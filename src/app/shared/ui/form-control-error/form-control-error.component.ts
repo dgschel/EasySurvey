660b226d958c8e631bb026b8db01c38ed10a5e08
dgschel <goesc_@hotmail.de>
2024-06-28 07:56:16 +0200

    receive error message

--- src/app/shared/ui/form-control-error/form-control-error.component.ts
+++ src/app/shared/ui/form-control-error/form-control-error.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-form-control-error',
   standalone: true,
   imports: [],
-  templateUrl: './form-control-error.component.html',
-  styleUrl: './form-control-error.component.scss'
+  template: `
+    <div class="p-1 text-sm text-error font-semibold">{{ message() }}</div>
+  `,
 })
 export class FormControlErrorComponent {
-
+  message = input<string>('');
 }
