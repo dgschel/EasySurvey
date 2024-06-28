0dff5b33756d3d1786171acbf475ea07193f3bd9
dgschel <goesc_@hotmail.de>
2024-06-28 07:56:06 +0200

    add component to display error

---
+++ src/app/shared/ui/form-control-error/form-control-error.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-control-error.component.html',
+  styleUrl: './form-control-error.component.scss'
+})
+export class FormControlErrorComponent {
+
+}
