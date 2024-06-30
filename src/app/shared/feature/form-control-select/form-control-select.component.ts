a2cfbd97d53c54726afcfde37ce68e4c0141bf6b
dgschel <goesc_@hotmail.de>
2024-06-30 18:43:19 +0200

    add validation and error class

--- src/app/shared/feature/form-control-select/form-control-select.component.ts
+++ src/app/shared/feature/form-control-select/form-control-select.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, input } from '@angular/core';
-import { ControlContainer, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { ControlContainer, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control-select',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './form-control-select.component.html',
   styleUrl: './form-control-select.component.scss',
   viewProviders: [{ // viewProviders is used to provide the parent form group to the child component. Useful for content projection
@@ -14,6 +15,7 @@ import { ControlContainer, FormBuilder, FormGroup, ReactiveFormsModule } from '@
 })
 export class FormControlSelectComponent {
   formControlKey = input.required<string>();
+  validatorsFn = input<ValidatorFn[]>();
   parentContainer = inject(ControlContainer);
 
   private fb = inject(FormBuilder);
@@ -22,8 +24,16 @@ export class FormControlSelectComponent {
     return this.parentContainer.control as FormGroup;
   }
 
+  get control() {
+    return this.parentFormGroup.get(this.formControlKey());
+  }
+
+  get isInvalidAndTouchedOrDirty() {
+    return this.control?.invalid && (this.control?.dirty || this.control?.touched);
+  }
+
   ngOnInit(): void {
-    this.parentFormGroup.addControl(this.formControlKey(), this.fb.control(''));
+    this.parentFormGroup.addControl(this.formControlKey(), this.fb.control('', { validators: this.validatorsFn(), updateOn: 'blur' }));
   }
 
   ngOnDestroy(): void {
