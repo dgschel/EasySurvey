449d5774842ee9e1805afed89a8eb53ccdfdfeaa
dgschel <goesc_@hotmail.de>
2024-06-30 18:54:02 +0200

    add error messages if any

--- src/app/shared/feature/form-control-select/form-control-select.component.ts
+++ src/app/shared/feature/form-control-select/form-control-select.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, input } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { ControlContainer, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
+import { FormControlErrorComponent } from '../../ui/form-control-error/form-control-error.component';
 
 @Component({
   selector: 'app-form-control-select',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, FormControlErrorComponent],
   templateUrl: './form-control-select.component.html',
   styleUrl: './form-control-select.component.scss',
   viewProviders: [{ // viewProviders is used to provide the parent form group to the child component. Useful for content projection
@@ -26,6 +27,11 @@ export class FormControlSelectComponent {
 
   get control() {
     return this.parentFormGroup.get(this.formControlKey());
+  }
+
+  get validationErrors(): string[] | null {
+    const errors = this.control?.errors
+    return errors ? Object.values(errors) : null;
   }
 
   get isInvalidAndTouchedOrDirty() {
