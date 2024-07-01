c9aa596777ceb07647b75d2d59d7861605ad115a
dgschel <goesc_@hotmail.de>
2024-07-01 12:05:18 +0200

    define a base class for a form control

---
+++ src/app/core/model/base-form-control.ts
@@ -0,0 +1,46 @@
+import { FormControl, FormGroup, ValidatorFn } from "@angular/forms";
+
+/**
+ * Represents a base form control
+ */
+interface IBaseFormControl {
+  formGroup: FormGroup;
+  validatorsFn: () => ValidatorFn[];
+}
+
+/**
+ * Represents a base form control.
+ */
+export class BaseFormControl implements IBaseFormControl {
+  /**
+   * Initializes a new instance of the BaseFormControl class.
+   * @param formGroup The parent form group.
+   * @param validatorsFn A function that returns an array of validator functions.
+   * @param controlName The name of the control.
+   */
+  constructor(public formGroup: FormGroup, public validatorsFn: () => ValidatorFn[], public controlName: string) {
+    this.formGroup.addControl(this.controlName, new FormControl('', { validators: this.validatorsFn(), updateOn: 'blur' }));
+  }
+
+  /**
+   * Gets the control associated with the form control.
+   */
+  get control() {
+    return this.formGroup.get(this.controlName);
+  }
+
+  /**
+   * Gets a value indicating whether the control is valid.
+   */
+  get isValid() {
+    return this.control?.invalid && (this.control?.dirty || this.control?.touched);
+  }
+
+  /**
+   * Gets the validation errors associated with the control.
+   */
+  get validationErrors() {
+    const errors = this.control?.errors;
+    return errors ? Object.values(errors) : null;
+  }
+}
