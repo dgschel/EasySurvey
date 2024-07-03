672310e3bebfbed17bbe61fd0a18d63b8ef55224
dgschel <goesc_@hotmail.de>
2024-07-03 10:13:23 +0200

    add new component

---
+++ src/app/shared/ui/dynamic-select-option/dynamic-select-option.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-select-option',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-select-option.component.html',
+  styleUrl: './dynamic-select-option.component.scss'
+})
+export class DynamicSelectOptionComponent {
+
+}
