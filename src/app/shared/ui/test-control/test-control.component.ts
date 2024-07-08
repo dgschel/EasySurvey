a5739961229fcf719f75b6e17267d1d9093b17a8
dgschel <goesc_@hotmail.de>
2024-07-08 21:14:02 +0200

    using viewProviders to inject parent form with skip self

--- src/app/shared/ui/test-control/test-control.component.ts
+++ src/app/shared/ui/test-control/test-control.component.ts
@@ -1,15 +1,17 @@
 import { NgIf } from '@angular/common';
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { ControlContainer, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, ElementRef, Inject, inject, Optional, SkipSelf, ViewChild } from '@angular/core';
+import { ControlContainer, FormArray, FormBuilder, FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-test-control',
   standalone: true,
   imports: [ReactiveFormsModule, NgIf],
   templateUrl: './test-control.component.html',
   styleUrl: './test-control.component.scss',
+  viewProviders: [{ provide: ControlContainer, useFactory: () => inject(ControlContainer, { skipSelf: true }) }]
 })
 export class TestControlComponent {
-  parentformContainer = inject(ControlContainer);
+  formParentContainer = inject(ControlContainer);
+  fb = inject(FormBuilder);
 
   section: FormGroup = this.fb.group({
     name: ''
@@ -17,8 +19,12 @@ export class TestControlComponent {
 
   @ViewChild('mySelect') mySelect!: ElementRef;
 
+  ngOnInit() {
+    this.sections.push(this.section);
+  }
+
   get form() {
-    return this.parentformContainer.control as FormGroup;
+    return this.formParentContainer.control as FormGroup;
   }
 
   get sections() {
@@ -27,10 +33,6 @@ export class TestControlComponent {
 
   get control() {
     return this.section.get('name') as FormControl;
-  }
-
-  constructor(private fb: FormBuilder) {
-    this.sections.push(this.section);
   }
 
   toggleRequired(checked: boolean) {
