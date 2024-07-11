df5bf8feeb65ef63f89ac5285664376e9f64057a
dgschel <goesc_@hotmail.de>
2024-07-11 11:42:57 +0200

    create component

---
+++ src/app/shared/feature/form-select-option/form-select-option.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-select-option',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-select-option.component.html',
+  styleUrl: './form-select-option.component.scss'
+})
+export class FormSelectOptionComponent {
+
+}
