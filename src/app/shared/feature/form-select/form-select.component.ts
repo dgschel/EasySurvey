9e370348f1111e3638e20fbbc8e7d81a56c966c5
dgschel <goesc_@hotmail.de>
2024-07-11 19:04:35 +0200

    create component to provide the options

---
+++ src/app/shared/feature/form-select/form-select.component.ts
@@ -0,0 +1,44 @@
+import { AfterViewInit, Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
+
+import { FormControlSelectComponent } from '../form-control-select/form-control-select.component';
+import { DynamicOptionComponent } from '../../ui/dynamic-option/dynamic-option.component';
+
+@Component({
+  selector: 'app-form-select',
+  standalone: true,
+  imports: [FormControlSelectComponent],
+  templateUrl: './form-select.component.html',
+  styleUrl: './form-select.component.scss',
+})
+export class FormSelectComponent implements AfterViewInit {
+  components: ComponentRef<DynamicOptionComponent>[] = [];
+
+  @ViewChild('host', { read: ViewContainerRef }) host!: ViewContainerRef;
+
+  // Will be used to populate options
+  get values() {
+    return this.components.map(cmp => cmp.instance.value);
+  }
+
+  ngAfterViewInit(): void {
+    this.addComponent();
+  }
+
+  addComponent() {
+    const componentRef = this.host.createComponent(DynamicOptionComponent);
+    componentRef.changeDetectorRef.detectChanges();
+    this.components.push(componentRef);
+    componentRef.instance.remove.subscribe(() => this.onRemove(componentRef));
+  }
+
+  onRemove(cmpRef: ComponentRef<DynamicOptionComponent>) {
+    // Only remove if there is more than one component
+    if (this.components.length === 1) return;
+
+    const index = this.components.indexOf(cmpRef);
+    if (index !== -1) {
+      this.components.splice(index, 1);
+      cmpRef.destroy();
+    }
+  }
+}
