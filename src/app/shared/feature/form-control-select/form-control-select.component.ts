ce0184134c00271fd3fd2a67e8516aebffe00aa8
dgschel <goesc_@hotmail.de>
2024-07-11 11:04:50 +0200

    copy code mostly from form input control

--- src/app/shared/feature/form-control-select/form-control-select.component.ts
+++ src/app/shared/feature/form-control-select/form-control-select.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { ControlContainer, FormGroup, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
+import { ControlContainer, FormArray, FormGroup, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
 
 import { FormControlErrorComponent } from '../../ui/form-control-error/form-control-error.component';
 import { SurveyFormControl } from '../../model/survey-form-control';
@@ -18,11 +18,11 @@ import { SurveyFormControl } from '../../model/survey-form-control';
 })
 export class FormControlSelectComponent {
   parentContainer = inject(ControlContainer);
-  controlKeyName = input.required<string>();
+  controlKeyName = input<string>();
   options = input<string[]>([]);
   label = input<string>();
   validatorsFn = input<ValidatorFn[]>();
-  surveyFormControl: SurveyFormControl | null = null;
+  surveyFormControl: SurveyFormControl | undefined;
 
   get parentFormGroup() {
     return this.parentContainer.control as FormGroup;
@@ -36,12 +36,24 @@ export class FormControlSelectComponent {
     return this.surveyFormControl?.validationErrors
   }
 
-  ngOnInit(): void {
-    this.surveyFormControl = new SurveyFormControl(this.parentFormGroup, this.validatorsFn(), this.controlKeyName());
+  get sections() {
+    return this.parentFormGroup.get('sections') as FormArray;
   }
 
+  get control() {
+    return this.surveyFormControl?.control;
+  }
+
+  ngOnInit(): void {
+    this.surveyFormControl = new SurveyFormControl(this.sections, this.validatorsFn(), this.controlKeyName());
+  }
 
   ngOnDestroy(): void {
-    this.parentFormGroup.removeControl(this.controlKeyName());
+    if (this.surveyFormControl?.fg) {
+      const formGroupIndex = this.sections.controls.indexOf(this.surveyFormControl.fg);
+      if (formGroupIndex !== -1) {
+        this.sections.removeAt(formGroupIndex);
+      }
+    }
   }
 }
