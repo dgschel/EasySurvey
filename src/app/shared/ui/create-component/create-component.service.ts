1ffb6a1b38a684335002752c59b274542423022c
dgschel <goesc_@hotmail.de>
2024-07-13 09:59:52 +0200

    add service to create components

--- src/app/shared/ui/create-component/create-component.service.ts
+++ src/app/shared/ui/create-component/create-component.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal, Type } from '@angular/core';
+import { FormControlType } from '../../../util/type/survey-type';
+import { createFormControlComponent } from '../../../util/component/create';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CreateComponentService {
+export class CreateComponentService<T> {
+  private _formControlType = signal<FormControlType>('input'); // default form control type
+  formControlTypeComputed = computed(() => this._formControlType());
 
-  constructor() { }
+  createFormControlComponentInstance(controlType: FormControlType = 'input'): Type<T> {
+    return createFormControlComponent<T>(controlType);
+  }
+
+  setFormControlType(controlType: FormControlType) {
+    this._formControlType.set(controlType);
+  }
 }
