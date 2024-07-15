39ab1f972124157ed975427a369162296c305a19
dgschel <goesc_@hotmail.de>
2024-07-15 14:09:04 +0200

    add service to build up single form with state

--- src/app/core/service/survey-data.service.ts
+++ src/app/core/service/survey-data.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { BaseSurveyFormControl } from '../model/base-form-control';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SurveyDataService {
+  surveyDataState = signal<SurveyDataModel[]>([]);
 
-  constructor() { }
+  saveModel(data: SurveyDataModel) {
+    this.surveyDataState.update(state => {
+      const index = state.findIndex(survey => survey.control === data.control);
+      if (index === -1) return [...state, data];
+
+      state[index] = data;
+      return state;
+    });
+    localStorage.setItem('surveyData', JSON.stringify(this.surveyDataState()));
+  }
+
+  removeModel(model: SurveyDataModel) {
+    const filtered = this.surveyDataState().filter(s => s.control !== model.control)
+    this.surveyDataState.set(filtered);
+    localStorage.setItem('surveyData', JSON.stringify(this.surveyDataState()));
+  }
+}
+
+export type SurveyDataModel = {
+  text: string;
+  required: boolean;
+  control: BaseSurveyFormControl;
 }
