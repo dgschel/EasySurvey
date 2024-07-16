8849a19f39902b43ba72ceeaf8ad447667016fbe
dgschel <goesc_@hotmail.de>
2024-07-16 11:34:04 +0200

    render dynamic component

--- src/app/shared/ui/view-survey-group/view-survey-group.component.ts
+++ src/app/shared/ui/view-survey-group/view-survey-group.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, Injector, input, ViewChild, ViewContainerRef } from '@angular/core';
+import { CreateComponentComponent } from '../create-component/create-component.component';
+import { FormControlComponentType, FormControlType } from '../../../util/type/survey-type';
+import { CreateComponentService } from '../create-component/create-component.service';
 
 @Component({
   selector: 'app-view-survey-group',
   standalone: true,
-  imports: [],
+  imports: [CreateComponentComponent],
+  providers: [CreateComponentService],
   templateUrl: './view-survey-group.component.html',
-  styleUrl: './view-survey-group.component.scss'
+  styleUrl: './view-survey-group.component.scss',
 })
-export class ViewSurveyGroupComponent {
+export class ViewSurveyGroupComponent implements AfterViewInit {
+  controlType = input.required<FormControlType>()
+  cmpService: CreateComponentService<FormControlComponentType>;
+  componentRef: ComponentRef<FormControlComponentType> | undefined;
+  @ViewChild('component', { read: ViewContainerRef }) componentContainer!: ViewContainerRef;
 
+  /**
+   * Creates an instance of CreateComponentComponent.
+   * @param injector - The injector used for dependency injection.
+  */
+  constructor(private injector: Injector) {
+    this.cmpService = this.injector.get(CreateComponentService); // Fully typed service instance
+  }
+
+  ngAfterViewInit(): void {
+    const cmp = this.cmpService.createFormControlComponentInstance(this.controlType())
+    this.componentContainer.clear();
+    this.componentRef = this.componentContainer.createComponent(cmp);
+    this.componentRef.changeDetectorRef.detectChanges();
+  }
 }
