f3d7b7638f043222f346c571d6de93d2e7a400d8
dgschel <goesc_@hotmail.de>
2024-07-17 20:41:15 +0200

    fix class to injectable service

--- src/app/core/service/survey-data-storage.service.ts
+++ src/app/core/service/survey-data-storage.service.ts
@@ -1,27 +1,30 @@
-import { ComponentRef } from "@angular/core";
+import { ComponentRef, Injectable } from "@angular/core";
 import { BehaviorSubject, map, Observable } from "rxjs";
 import { SurveyBaseType, SurveyRefData } from "../../util/type/survey-type";
 import { CreateSurveyGroupComponent } from "../../shared/feature/create-survey-group/create-survey-group.component";
 
-export class SurveyDataStorage {
-  private dataSubject = new BehaviorSubject<SurveyRefData[]>([]);
-  data$ = this.dataSubject.asObservable();
+@Injectable({
+  providedIn: 'root'
+})
+export class SurveyDataStorageService {
+  private dataStorageSubject = new BehaviorSubject<SurveyRefData[]>([]);
+  data$ = this.dataStorageSubject.asObservable();
 
   addData(surveyData: SurveyRefData) {
-    const currentDatas = this.dataSubject.value;
-    this.dataSubject.next([...currentDatas, surveyData])
+    const currentDatas = this.dataStorageSubject.value;
+    this.dataStorageSubject.next([...currentDatas, surveyData])
   }
 
   updateData(ref: ComponentRef<CreateSurveyGroupComponent>, state: SurveyRefData) {
-    const updatedData = this.dataSubject.value.map(current => {
+    const updatedData = this.dataStorageSubject.value.map(current => {
       return current.ref === ref ? { ...current, data: state.data } : current;
     });
-    this.dataSubject.next(updatedData);
+    this.dataStorageSubject.next(updatedData);
   }
 
   removeData(ref: ComponentRef<CreateSurveyGroupComponent>) {
-    const filteredData = this.dataSubject.value.filter(current => current.ref !== ref);
-    this.dataSubject.next(filteredData);
+    const filteredData = this.dataStorageSubject.value.filter(current => current.ref !== ref);
+    this.dataStorageSubject.next(filteredData);
   };
 
   getData$(): Observable<SurveyBaseType[]> {
--- src/app/core/service/survey-data-storage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SurveyDataStorage } from './survey-data-storage';
-
-describe('SurveyDataStorage', () => {
-  it('should create an instance', () => {
-    expect(new SurveyDataStorage()).toBeTruthy();
-  });
-});
