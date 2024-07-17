0b92b0195a5f03c81dba20bad63b0ce22459b65a
dgschel <goesc_@hotmail.de>
2024-07-17 20:17:31 +0200

    add new component

---
+++ src/app/shared/feature/create-survey-group/create-survey-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-survey-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-survey-group.component.html',
+  styleUrl: './create-survey-group.component.scss'
+})
+export class CreateSurveyGroupComponent {
+
+}
