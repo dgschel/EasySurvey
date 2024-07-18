66e313a308af5c83c2613c7db47c33d8f30ac052
dgschel <goesc_@hotmail.de>
2024-07-18 11:26:19 +0200

    signal member for validator record

--- src/app/core/model/survey-base.ts
+++ src/app/core/model/survey-base.ts
@@ -1,16 +1,14 @@
 import { signal, computed } from "@angular/core"
-import { SurveyBaseType } from "../../util/type/survey-type"
-import { customRequiredValidator } from "../../shared/form-validator/validators"
+import { SurveyBaseType, SurveyValidatorMap } from "../../util/type/survey-type"
 
 export class SurveyBase {
   title = signal<string>('')
   description = signal<string>('')
+  validatorMap = signal<SurveyValidatorMap>({} as SurveyValidatorMap);
 
   state = computed<SurveyBaseType>(() => ({
     title: this.title(),
     description: this.description(),
-    validator: {
-      required: () => customRequiredValidator
-    }
+    validator: this.validatorMap()
   }))
 }
