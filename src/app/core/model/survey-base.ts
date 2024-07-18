828a89e0e8d95ec22a35efa2065f66e959a8bb76
dgschel <goesc_@hotmail.de>
2024-07-18 06:27:32 +0200

    removed boolean from base and add validators WIP

--- src/app/core/model/survey-base.ts
+++ src/app/core/model/survey-base.ts
@@ -4,10 +4,10 @@ import { SurveyBaseType } from "../../util/type/survey-type"
 export class SurveyBase {
   title = signal<string>('')
   description = signal<string>('')
-  hasDescription = signal<boolean>(false)
 
   state = computed<SurveyBaseType>(() => ({
     title: this.title(),
-    description: this.description()
+    description: this.description(),
+    validators: []
   }))
 }
