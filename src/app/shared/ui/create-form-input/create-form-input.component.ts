b5186305d78cae17a4b85343599b06309ce2f0c7
dgschel <goesc_@hotmail.de>
2024-07-20 15:44:50 +0200

    add basic input to display

---
+++ src/app/shared/ui/create-form-input/create-form-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-form-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-form-input.component.html',
+  styleUrl: './create-form-input.component.scss'
+})
+export class CreateFormInputComponent {
+
+}
