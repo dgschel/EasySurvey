dc3d33450e3afe148d083a4c34adbb99234bac66
dgschel <goesc_@hotmail.de>
2024-07-20 15:39:23 +0200

    add type and restrict T to basetype

--- src/app/core/model/survey-base.ts
+++ src/app/core/model/survey-base.ts
@@ -1,14 +1,15 @@
 import { signal, computed } from "@angular/core"
-import { SurveyBaseType, SurveyValidatorMap } from "../../util/type/survey-type"
+import { FormControlType, SurveyBaseType, SurveyValidatorMap } from "../../util/type/survey-type"
 
-export class SurveyBase {
-  title = signal<string>('')
-  description = signal<string>('')
+export class SurveyBase<T extends SurveyBaseType> {
+  type = signal<FormControlType>('input');
+  title = signal<string>('');
+  description = signal<string>('');
   validatorMap = signal<SurveyValidatorMap>({} as SurveyValidatorMap);
 
-  state = computed<SurveyBaseType>(() => ({
+  state = computed<T>(() => ({
     title: this.title(),
     description: this.description(),
     validator: this.validatorMap()
-  }))
+  }) as T);
 }
