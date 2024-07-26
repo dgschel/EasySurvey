099b131cb16826f666c0512d35d52fe3f311dbc5
dgschel <goesc_@hotmail.de>
2024-07-26 14:27:11 +0200

    use new validation configuration

--- src/app/shared/model/survey-form-control.ts
+++ src/app/shared/model/survey-form-control.ts
@@ -1,10 +1,11 @@
 import { FormArray } from "@angular/forms";
 import { BaseSurveyFormControl } from "../../core/model/base-form-control";
-import { SurveyValidatorMap } from "../../util/type/survey-type";
+import { SurveyValidatorType, ValidatorConfig } from "../../util/type/survey-type";
+import { surveyValidatorMap } from "../form-validator/validators";
 
 export class SurveyFormControl extends BaseSurveyFormControl {
-  constructor(form: FormArray, validator: SurveyValidatorMap, controlName: string = 'controlName') {
-    const validatorsFn = Object.values(validator)
-    super(form, () => validatorsFn, controlName);
+  constructor(form: FormArray, validator: ValidatorConfig, controlName: string = 'controlName') {
+    const validators = Object.keys(validator).map((key => surveyValidatorMap[key as SurveyValidatorType](validator)));
+    super(form, () => validators, controlName);
   }
 }
