eaddd1e3880b374643d5b9f9a403afab67a082bd
dgschel <goesc_@hotmail.de>
2024-07-29 19:44:24 +0200

    concrete implementation of a survey template

---
+++ src/app/core/templates/purchase-survey-template.ts
@@ -0,0 +1,44 @@
+import { SurveyInputModel, SurveySelectModel, SurveyModel } from "../../util/type/survey-type";
+import { SurveyTemplateContract } from "../types/survey-template-contract";
+
+export class PurchaseSurveyTemplate implements SurveyTemplateContract {
+  createSurveyInput(model?: SurveyInputModel): SurveyInputModel {
+    return model
+      ? { ...model }
+      : {
+        title: 'Purchase Input Title',
+        description: 'Purchase Input Description',
+        type: 'input',
+        placeholder: 'Purchase Input Placeholder',
+        validator: {}
+      };
+  }
+
+  createSurveySelect(model?: SurveySelectModel): SurveySelectModel {
+    return model
+      ? { ...model }
+      : {
+        title: 'Purchase Select Title',
+        description: 'Purchase Select Description',
+        type: 'select',
+        options: ["Option 1", "Option 2"],
+        validator: {}
+      };
+  }
+
+  createPredefinedSurvey(): SurveyModel[] {
+    const initialSurveyInput = this.createSurveyInput();
+    const secondSurveyInput = this.createSurveyInput({ ...initialSurveyInput, title: 'Second Purchase Input Title' });
+    const thirdSurveyInput = this.createSurveyInput({ ...secondSurveyInput, title: 'Third Purchase Input Title', placeholder: 'Third Purchase Input Placeholder' });
+
+    const initialSurveySelect = this.createSurveySelect();
+    const secondSurveySelect = this.createSurveySelect({ ...initialSurveySelect, title: 'Second Purchase Select Title' });
+    const thirdSurveySelect = this.createSurveySelect({ ...secondSurveySelect, title: 'Third Purchase Select Title', options: ['Option 1', 'Option 2', 'Option 3'] });
+
+
+    const inputs = [initialSurveyInput, secondSurveyInput, thirdSurveyInput];
+    const selects = [initialSurveySelect, secondSurveySelect, thirdSurveySelect];
+
+    return [...inputs, ...selects];
+  }
+}
