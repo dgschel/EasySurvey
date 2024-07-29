7a793b491c98b1e6f6d68deba54b4f0a8187aace
dgschel <goesc_@hotmail.de>
2024-07-29 19:44:40 +0200

    add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ViewSurveyFormComponent } from './view-survey-form/view-survey-form.component';
+import { StoreFormComponent } from './store-form/store-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'viewform', component: ViewSurveyFormComponent }
+  { path: 'viewform', component: ViewSurveyFormComponent },
+  { path: 'store', component: StoreFormComponent },
 ];
