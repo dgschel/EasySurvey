7a8011c67c42593ad0fca18f6cd7d0683b071ea4
dgschel <goesc_@hotmail.de>
2024-07-30 14:46:05 +0200

    edit naming

--- src/app/store-form/store-form.component.ts
+++ src/app/store-form/store-form.component.ts
@@ -5,11 +5,12 @@ import { ViewSurveyGroupComponent } from '../shared/ui/view-survey-group/view-su
 import { BasicSurveyTemplate } from '../core/templates/basic-survey-template';
 import { PurchaseSurveyTemplate } from '../core/templates/purchase-survey-template';
 import { SurveyTemplateManager } from '../core/model/survey-template-manager';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-store-form',
   standalone: true,
-  imports: [ReactiveFormsModule, ViewSurveyGroupComponent],
+  imports: [ReactiveFormsModule, ViewSurveyGroupComponent, KeyValuePipe],
   templateUrl: './store-form.component.html',
   styleUrl: './store-form.component.scss'
 })
@@ -17,17 +18,20 @@ export class StoreFormComponent {
   surveyManager: SurveyTemplateManager;
   surveyNames: string[] = [];
   survey: Record<string, number> = {};
-  total: number = 0;
+
+  templateModelCounts: Record<string, Record<string, number>> = {}
 
   constructor() {
     const basicSurvey = new BasicSurveyTemplate();
     const purchaseSurvey = new PurchaseSurveyTemplate();
 
     this.surveyManager = new SurveyTemplateManager();
-    this.surveyManager.addSurvey('basic', basicSurvey);
-    this.surveyManager.addSurvey('purchase', purchaseSurvey);
+    this.surveyManager.addSurvey('Standard', basicSurvey);
+    this.surveyManager.addSurvey('Einkaufsformular', purchaseSurvey);
     this.surveyNames = this.surveyManager.surveyNames;
     this.survey = this.surveyManager.countControlsBySurvey;
+
+    this.templateModelCounts = this.surveyManager.getTemplateModelCounts();
   }
 }
 
