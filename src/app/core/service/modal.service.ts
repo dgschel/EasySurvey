2fc6155f35cf656e629b677e3789aef62a55577e
dgschel <goesc_@hotmail.de>
2024-07-31 09:18:17 +0200

    add logic to handle open and close modal and insert any content

--- src/app/core/service/modal.service.ts
+++ src/app/core/service/modal.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, ComponentRef, createComponent, EnvironmentInjector, Injectable, TemplateRef } from '@angular/core';
+import { ModalComponent } from '../component/modal/modal.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+  modalCompRef: ComponentRef<ModalComponent> | undefined;
 
-  constructor() { }
+  constructor(
+    private appRef: ApplicationRef,
+    private injector: EnvironmentInjector
+  ) { }
+
+  createComponent(content: TemplateRef<any>) {
+    // Create an embedded view from the template
+    const modalEmbeddedView = content.createEmbeddedView(null);
+
+    // Create the base modal component
+    this.modalCompRef = createComponent(ModalComponent, {
+      environmentInjector: this.injector, // Pass the injector to the component
+      projectableNodes: [modalEmbeddedView.rootNodes], // Pass the nodes to the component to inject the content inside of <ng-template>
+    });
+
+    // Append the component to the body
+    document.body.appendChild(this.modalCompRef.location.nativeElement);
+
+    // Attach the component to the application so it can be checked by angular
+    this.appRef.attachView(this.modalCompRef.hostView);
+
+    return this.modalCompRef;
+  }
+
+  open(templateRef: TemplateRef<any>): ComponentRef<ModalComponent> {
+    return this.createComponent(templateRef);
+  }
+
+  close(): void {
+    this.modalCompRef?.destroy();
+    if (this.modalCompRef?.hostView) {
+      this.appRef.detachView(this.modalCompRef?.hostView);
+    }
+  }
 }
