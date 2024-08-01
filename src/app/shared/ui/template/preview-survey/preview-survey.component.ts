2140851eba30b6fd4c7156b506b148ebc11f399f
dgschel <goesc_@hotmail.de>
2024-08-01 09:47:04 +0200

    create new template for a preview of a survey

---
+++ src/app/shared/ui/template/preview-survey/preview-survey.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-survey',
+  standalone: true,
+  imports: [],
+  templateUrl: './preview-survey.component.html',
+  styleUrl: './preview-survey.component.scss'
+})
+export class PreviewSurveyComponent {
+
+}
