1ea0744c4c26aab5953333940e4f766af1ab8e84
dgschel <goesc_@hotmail.de>
2024-08-01 11:10:01 +0200

    delete test data

--- src/app/core/service/survey-data.service.ts
+++ src/app/core/service/survey-data.service.ts
@@ -10,75 +10,58 @@ export class SurveyDataService {
   getSurveyData(): SurveyModelStorage[] {
     const storageData = localStorage.getItem('surveyData') ?? "";
     const storage = JSON.parse(storageData) as SurveyModelStorage[];
-
-    const entries = storage.map((entry: SurveyModelStorage) => {
-      const validator = Object.keys(entry.validator).reduce((acc, curr) => {
-        const validatorConfig = surveyValidatorMap[curr as SurveyValidatorType];
-        const validatorFn = validatorConfig(entry.validator);
-        return {
-          ...acc,
-          [curr]: validatorFn
-        }
-      }, {});
-
-      return {
-        ...entry,
-        validator
-      }
-    });
-
-    return entries;
+    return storage;
   }
 
-  getTestSurveyData(): SurveyModel[] {
-    const inputs: OmitValidator<SurveyInputModel>[] = [
-      {
-        type: 'input',
-        placeholder: 'Enter your name2',
-        description: 'Please enter your name',
-        title: 'Name',
-        validator: [customRequiredValidator('Name is required 2'), customMinLengthValidator(3, 'Min length of 3 characters')]
-      },
-      {
-        type: 'input',
-        placeholder: 'Enter your name3',
-        description: 'Please enter your name',
-        title: 'Name',
-        validator: [customRequiredValidator('Name is required 3'), customMinLengthValidator(3, 'Min length of 3 characters')]
-      },
-      {
-        type: 'input',
-        placeholder: 'Enter your name4',
-        description: 'Please enter your name',
-        title: 'Name',
-        validator: [customRequiredValidator('Name is required 4'), customMinLengthValidator(3, 'Min length of 3 characters')]
-      }
-    ]
+  // getTestSurveyData(): SurveyModel[] {
+  //   const inputs: OmitValidator<SurveyInputModel>[] = [
+  //     {
+  //       type: 'input',
+  //       placeholder: 'Enter your name2',
+  //       description: 'Please enter your name',
+  //       title: 'Name',
+  //       validator: [customRequiredValidator('Name is required 2'), customMinLengthValidator(3, 'Min length of 3 characters')]
+  //     },
+  //     {
+  //       type: 'input',
+  //       placeholder: 'Enter your name3',
+  //       description: 'Please enter your name',
+  //       title: 'Name',
+  //       validator: [customRequiredValidator('Name is required 3'), customMinLengthValidator(3, 'Min length of 3 characters')]
+  //     },
+  //     {
+  //       type: 'input',
+  //       placeholder: 'Enter your name4',
+  //       description: 'Please enter your name',
+  //       title: 'Name',
+  //       validator: [customRequiredValidator('Name is required 4'), customMinLengthValidator(3, 'Min length of 3 characters')]
+  //     }
+  //   ]
 
-    const selects: OmitValidator<SurveySelectModel>[] = [
-      {
-        type: 'select',
-        options: ['Option 1', 'Option 2', 'Option 3'],
-        description: 'Please select an option',
-        title: 'Select an option',
-        validator: [customRequiredValidator('Option is required')]
-      },
-      {
-        type: 'select',
-        options: ['Option 1', 'Option 2', 'Option 3'],
-        description: 'Please select an option',
-        title: 'Select an option',
-        validator: [customRequiredValidator('Option is required')]
-      },
-      {
-        type: 'select',
-        options: ['Option 1', 'Option 2', 'Option 3'],
-        description: 'Please select an option',
-        title: 'Select an option',
-        validator: [customRequiredValidator('Option is required')]
-      }
-    ];
+  //   const selects: OmitValidator<SurveySelectModel>[] = [
+  //     {
+  //       type: 'select',
+  //       options: ['Option 1', 'Option 2', 'Option 3'],
+  //       description: 'Please select an option',
+  //       title: 'Select an option',
+  //       validator: [customRequiredValidator('Option is required')]
+  //     },
+  //     {
+  //       type: 'select',
+  //       options: ['Option 1', 'Option 2', 'Option 3'],
+  //       description: 'Please select an option',
+  //       title: 'Select an option',
+  //       validator: [customRequiredValidator('Option is required')]
+  //     },
+  //     {
+  //       type: 'select',
+  //       options: ['Option 1', 'Option 2', 'Option 3'],
+  //       description: 'Please select an option',
+  //       title: 'Select an option',
+  //       validator: [customRequiredValidator('Option is required')]
+  //     }
+  //   ];
 
-    return [...inputs, ...selects] as SurveyModel[];
-  }
+  //   return [...inputs, ...selects] as SurveyModel[];
+  // }
 }
