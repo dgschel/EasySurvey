98babd618371ac8e4233732ab6ce3921a37554c7
dgschel <goesc_@hotmail.de>
2024-08-01 09:52:58 +0200

    add input

--- src/app/shared/ui/template/preview-survey/preview-survey.component.ts
+++ src/app/shared/ui/template/preview-survey/preview-survey.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SurveyName } from '../../../../util/type/survey-type';
+import { ViewSurveyGroupComponent } from '../../view-survey-group/view-survey-group.component';
 
 @Component({
   selector: 'app-preview-survey',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ViewSurveyGroupComponent],
   templateUrl: './preview-survey.component.html',
   styleUrl: './preview-survey.component.scss'
 })
 export class PreviewSurveyComponent {
-
+  @Input() surveyName: SurveyName = 'Standard';
 }
