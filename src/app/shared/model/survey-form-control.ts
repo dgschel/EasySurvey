23e43e46bf7e3ec0b3ce2880a7687cf66dae70e3
dgschel <goesc_@hotmail.de>
2024-08-01 09:51:08 +0200

    use helper to create validator functions from config

--- src/app/shared/model/survey-form-control.ts
+++ src/app/shared/model/survey-form-control.ts
@@ -1,10 +1,11 @@
-import { FormArray, ValidatorFn } from "@angular/forms";
+import { FormArray } from "@angular/forms";
 import { BaseSurveyFormControl } from "../../core/model/base-form-control";
-import { SurveyValidatorFn } from "../../util/type/survey-type";
+import { ValidatorConfig } from "../../util/type/survey-type";
+import { createValidators } from "../form-validator/validators";
 
 export class SurveyFormControl extends BaseSurveyFormControl {
-  constructor(form: FormArray, validator: SurveyValidatorFn, controlName: string = 'controlName') {
-    const validatorFunctions = Object.values(validator);
-    super(form, () => validatorFunctions, controlName);
+  constructor(form: FormArray, validator: Partial<ValidatorConfig>, controlName: string = 'controlName') {
+    const validators = createValidators(validator);
+    super(form, () => validators, controlName);
   }
 }
