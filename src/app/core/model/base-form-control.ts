25761326842517fb185d9b586aaef4bedc463b33
dgschel <goesc_@hotmail.de>
2024-08-01 09:48:05 +0200

    call function to get array of validatorFns

--- src/app/core/model/base-form-control.ts
+++ src/app/core/model/base-form-control.ts
@@ -13,7 +13,7 @@ export class BaseSurveyFormControl {
    * @param controlName The name of the control.
    */
   constructor(public formArray: FormArray, public validatorsFn: () => ValidatorFn[], public controlName: string) {
-    const control = new FormControl('', { validators: validatorsFn.bind(this)(), updateOn: 'blur' })
+    const control = new FormControl('', { validators: validatorsFn(), updateOn: 'blur' })
     this.fg.addControl(controlName, control);
     this.formArray.push(this.fg);
   }
