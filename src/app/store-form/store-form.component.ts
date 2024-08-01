56dab47def513b0289baaf9160649de2d0422f06
dgschel <goesc_@hotmail.de>
2024-08-01 11:26:31 +0200

    create the component that gets inserted into modal

--- src/app/store-form/store-form.component.ts
+++ src/app/store-form/store-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, ComponentRef, inject, TemplateRef, ViewChild } from '@angular/core';
-import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, ComponentRef, createComponent, EnvironmentInjector, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { KeyValuePipe } from '@angular/common';
 
 import { ViewSurveyGroupComponent } from '../shared/ui/view-survey-group/view-survey-group.component';
@@ -7,8 +7,9 @@ import { BasicSurveyTemplate } from '../core/templates/basic-survey-template';
 import { PurchaseSurveyTemplate } from '../core/templates/purchase-survey-template';
 import { SurveyTemplateManager } from '../core/model/survey-template-manager';
 import { ModalService } from '../core/service/modal.service';
-import { SurveyModel, SurveyName } from '../util/type/survey-type';
 import { ModalComponent } from '../core/component/modal/modal.component';
+import { PreviewSurveyComponent } from '../shared/ui/template/preview-survey/preview-survey.component';
+import { SurveyName } from '../util/type/survey-type';
 
 @Component({
   selector: 'app-store-form',
@@ -19,19 +20,9 @@ import { ModalComponent } from '../core/component/modal/modal.component';
 })
 export class StoreFormComponent {
   modalService = inject(ModalService);
+  environmentInjector = inject(EnvironmentInjector);
   surveyManager: SurveyTemplateManager;
-  models: SurveyModel[] = [];
-
   modalRef!: ComponentRef<ModalComponent>;
-
-  // Use form to display the survey that is created by the user and inserted into the modal
-  // The form is not used to submit the survey, it is only used to display the survey
-  // We need this here so that the component, which is reponsible to create the survey based on the array of models, can be used in the template
-  form = new FormGroup({
-    sections: new FormArray([])
-  })
-
-  @ViewChild('container') container!: TemplateRef<any>;
 
   constructor() {
     const basicSurvey = new BasicSurveyTemplate();
@@ -43,9 +34,12 @@ export class StoreFormComponent {
   }
 
   preview(surveyName: string) {
-    const survey = this.surveyManager.getSurvey(surveyName as SurveyName);
-    this.models = survey.createPredefinedSurvey()
-    this.modalRef = this.modalService.open(this.container);
-    this.modalRef.instance.modalCloseEvent.subscribe(() => this.modalService.close());
+    const cmp = createComponent(PreviewSurveyComponent, {
+      environmentInjector: this.environmentInjector,
+    })
+
+    cmp.setInput('surveyName', surveyName as SurveyName);
+
+    const modalRef = this.modalService.open(cmp);
   }
 }
