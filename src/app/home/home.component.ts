37f03562bd6c33cc19a05c3f255994fa81e1118a
dgschel <goesc_@hotmail.de>
2024-08-05 12:20:13 +0200

    remove initialization of reactive form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { SurveyFormComponent } from '../shared/feature/survey-form/survey-form.component';
 import { SurveyDataService } from '../core/service/survey-data.service';
-import { ActivatedRoute } from '@angular/router';
 import { SurveyModel } from '../util/type/survey-type';
-import { FormGroup, FormArray, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule, SurveyFormComponent],
+  imports: [SurveyFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   surveyDataService = inject(SurveyDataService);
   route = inject(ActivatedRoute);
-
+  
   models: SurveyModel[] = [];
-
-  form = new FormGroup({
-    sections: new FormArray([])
-  })
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
