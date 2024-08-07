11670ded1c4aaef8d3f9585233028227e5ead043
dgschel <goesc_@hotmail.de>
2024-08-07 10:33:36 +0200

    remove localstorage functionality

--- frontend/src/app/shared/feature/survey-form/survey-form.component.ts
+++ frontend/src/app/shared/feature/survey-form/survey-form.component.ts
@@ -1,27 +1,26 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ComponentRef, inject, Input, ViewChild, ViewContainerRef } from '@angular/core';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ComponentRef, inject, Input, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { SurveyDataStorageService } from '../../../core/service/survey-data-storage.service';
 import { CreateSurveyGroupComponent } from '../create-survey-group/create-survey-group.component';
 import { SurveyModel } from '../../../util/type/survey-type';
+import { AzureSurveyService } from '../../../core/service/azure-survey.service';
 
 @Component({
   selector: 'app-survey-form',
   standalone: true,
-  imports: [JsonPipe, CreateSurveyGroupComponent, AsyncPipe],
+  imports: [CreateSurveyGroupComponent],
   templateUrl: './survey-form.component.html',
   styleUrl: './survey-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SurveyFormComponent implements AfterViewInit {
   private surveyStorage = inject(SurveyDataStorageService)
+  private azureSurveyService = inject(AzureSurveyService)
+
   @ViewChild('component', { read: ViewContainerRef }) componentContainer!: ViewContainerRef;
   @Input() models: SurveyModel[] = [];
+
   cmpRefs: ComponentRef<CreateSurveyGroupComponent>[] = [];
-
-  cdr = inject(ChangeDetectorRef)
-
-  data$ = this.surveyStorage.getData$();
 
   ngAfterViewInit(): void {
     this.models.forEach((model) => {
@@ -43,7 +42,7 @@ export class SurveyFormComponent implements AfterViewInit {
 
   save() {
     const models = this.cmpRefs.map((cmpRef) => cmpRef.instance.surveyModel())
-    localStorage.setItem('surveyData', JSON.stringify(models));
+    this.azureSurveyService.saveSurveyData(models).pipe().subscribe(data => console.log("Data saved", data));
     this.surveyStorage.clearData();
   }
 
