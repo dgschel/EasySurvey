defef777afb9d554e40c6aa9cd57415259c4d749
dgschel <goesc_@hotmail.de>
2024-08-07 13:25:38 +0200

    implement basic save and read operation

--- frontend/src/app/core/service/azure-survey.service.ts
+++ frontend/src/app/core/service/azure-survey.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { environment } from '../../../environments/environment.development';
+import { SurveyModel } from '../../util/type/survey-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AzureSurveyService {
+  constructor(private httpService: HttpClient) { }
 
-  constructor() { }
+  fetchSurveyData(id: string) {
+    return this.httpService.get<{ message: string, data: SurveyModel[] }>(environment.endpoints.readSurvey, { params: { id } });
+  }
+
+  saveSurveyData(data: SurveyModel[]) {
+    return this.httpService.post<{ message: string }>(environment.endpoints.saveSurvey, data);
+  }
 }
