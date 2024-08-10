b89605d56598c911d24930db892cc82730102dc8
dgschel <goesc_@hotmail.de>
2024-08-10 08:49:08 +0200

    add radio component

---
+++ frontend/src/app/shared/feature/form-control-radio/form-control-radio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control-radio',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-control-radio.component.html',
+  styleUrl: './form-control-radio.component.scss'
+})
+export class FormControlRadioComponent {
+
+}
