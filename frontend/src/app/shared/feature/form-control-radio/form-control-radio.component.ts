7bc280fe7e3e37fc1c4e67a81c3c4077f3b741af
dgschel <goesc_@hotmail.de>
2024-08-10 09:55:21 +0200

    create the structure of a radio element

--- frontend/src/app/shared/feature/form-control-radio/form-control-radio.component.ts
+++ frontend/src/app/shared/feature/form-control-radio/form-control-radio.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { ControlContainer, FormGroup, FormArray, ReactiveFormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+
+import { ValidatorConfig } from '../../../util/type/survey-type';
+import { SurveyFormControl } from '../../model/survey-form-control';
+import { FormControlErrorComponent } from '../../ui/form-control-error/form-control-error.component';
 
 @Component({
   selector: 'app-form-control-radio',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, FormControlErrorComponent],
   templateUrl: './form-control-radio.component.html',
-  styleUrl: './form-control-radio.component.scss'
+  styleUrl: './form-control-radio.component.scss',
+  viewProviders: [{ // viewProviders is used to provide the parent form group to the child component. Useful for content projection
+    provide: ControlContainer,
+    useFactory: () => inject(ControlContainer, { skipSelf: true })
+  }]
 })
 export class FormControlRadioComponent {
+  parentContainer = inject(ControlContainer);
+  controlKeyName = input<string>();
+  options = input<string[]>([]);
+  name = input.required<string>();
+  label = input<string>();
+  validator = input.required<Partial<ValidatorConfig>>({});
+  surveyFormControl: SurveyFormControl | undefined;
 
+  get parentFormGroup() {
+    return this.parentContainer.control as FormGroup;
+  }
+
+  get isFormControlValid() {
+    return this.surveyFormControl?.isValid
+  }
+
+  get validationErrors() {
+    return this.surveyFormControl?.validationErrors
+  }
+
+  get sections() {
+    return this.parentFormGroup.get('sections') as FormArray;
+  }
+
+  get control() {
+    return this.surveyFormControl?.control;
+  }
+
+  ngOnInit(): void {
+    this.surveyFormControl = new SurveyFormControl(this.sections, this.validator(), this.controlKeyName());
+  }
+
+  ngOnDestroy(): void {
+    if (this.surveyFormControl?.formGroup) {
+      const formGroupIndex = this.sections.controls.indexOf(this.surveyFormControl.formGroup);
+      if (formGroupIndex !== -1) {
+        this.sections.removeAt(formGroupIndex);
+      }
+    }
+  }
 }
