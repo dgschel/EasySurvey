208c5b98306795e64944b7c6ae3c7e46538e9d79
dgschel <goesc_@hotmail.de>
2024-08-12 10:43:11 +0200

    moved working part to new class

--- frontend/src/app/shared/feature/form-control-checkbox/form-control-checkbox.component.ts
+++ frontend/src/app/shared/feature/form-control-checkbox/form-control-checkbox.component.ts
@@ -1,11 +1,10 @@
 import { NgClass } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
-import { ControlContainer, FormArray, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { ControlContainer, FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { FormControlErrorComponent } from '../../ui/form-control-error/form-control-error.component';
 import { ValidatorConfig } from '../../../util/type/survey-type';
-import { CheckboxArrayFormControl, SurveyCheckboxFormControl, SurveyFormControl } from '../../model/survey-form-control';
-import { map } from 'rxjs';
+import { SurveyFormCheckboxControl, SurveyFormControl } from '../../model/survey-form-control';
 
 @Component({
   selector: 'app-form-control-checkbox',
@@ -38,29 +37,8 @@ export class FormControlCheckboxComponent {
     return this.surveyFormControl?.validationErrors
   }
 
-  get sections() {
-    return this.parentFormGroup.get('sections') as FormArray;
-  }
-
   get control() {
-    return this.surveyFormControl?.control;
-  }
-
-  get checkboxes(): FormArray<FormControl> {
-    const lastSectionIndex = this.sections.length - 1;
-    const lastSection = this.sections.at(lastSectionIndex) as FormGroup;
-    console.log(this.surveyFormControl?.control.get(this.controlKeyName()) as FormArray)
-    return lastSection.get(this.controlKeyName()) as FormArray;
-
-    return this.surveyFormControl?.control.get(this.controlKeyName()) as FormArray;
-  }
-
-  onCheckboxChange(e: Event, index: number) {
-    const checkboxes = this.checkboxes;
-    const checkbox = checkboxes.at(index);
-    const value = this.options()[index];
-    const isChecked = (e.target as HTMLInputElement).checked;
-    // checkbox.setValue(isChecked ? value : false);
+    return this.surveyFormControl?.control
   }
 
   // TODO: Add this to our validator config either as a new function or if required is set, then use this validator for checkboxes
@@ -77,25 +55,11 @@ export class FormControlCheckboxComponent {
   }
 
   ngOnInit(): void {
-    const controls = this.options().map(() => new FormControl(false));
-    const array = new FormArray(controls, { validators: this.minSelectedCheckboxes(), updateOn: 'blur' });
-
-    this.surveyFormControl = new CheckboxArrayFormControl(array, () => [], this.controlKeyName(), this.options());
-
-    this.surveyFormControl.formGroup.addControl(this.controlKeyName(), array);
-    console.log(this.surveyFormControl.control as any as FormArray)
+    this.surveyFormControl = new SurveyFormCheckboxControl(this.parentFormGroup, () => [], this.controlKeyName(), this.options());
   }
 
-  get formControlArray() {
+  formControlArray() {
+    // this.surveyFormControl.
     return this.surveyFormControl?.control as any as FormArray<FormControl<boolean>>
-  }
-
-  ngOnDestroy(): void {
-    if (this.surveyFormControl?.formGroup) {
-      const formGroupIndex = this.sections.controls.indexOf(this.surveyFormControl.formGroup);
-      if (formGroupIndex !== -1) {
-        this.sections.removeAt(formGroupIndex);
-      }
-    }
   }
 }
