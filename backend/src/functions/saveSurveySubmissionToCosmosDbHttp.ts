01db934519a729b1356f8b75aac07d5ae4651044
dgschel <goesc_@hotmail.de>
2024-08-21 18:16:16 +0200

    set output connection to cosmos db to create new doc

--- backend/src/functions/saveSurveySubmissionToCosmosDbHttp.ts
+++ backend/src/functions/saveSurveySubmissionToCosmosDbHttp.ts
@@ -1,4 +1,11 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, InvocationContext, output } from "@azure/functions";
+
+const cosmosOutput = output.cosmosDB({
+    databaseName: 'SurveyDB',
+    containerName: 'Submission',
+    connection: 'cosmosDbConnection',
+    partitionKey: '/surveyId',
+});
 
 export async function saveSurveySubmissionToCosmosDbHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
@@ -11,5 +18,6 @@ export async function saveSurveySubmissionToCosmosDbHttp(request: HttpRequest, c
 app.http('saveSurveySubmissionToCosmosDbHttp', {
     methods: ['POST'],
     authLevel: 'function',
+    extraOutputs: [cosmosOutput],
     handler: saveSurveySubmissionToCosmosDbHttp
 });
