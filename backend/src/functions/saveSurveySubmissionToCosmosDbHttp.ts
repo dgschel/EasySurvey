90234aad67f9fc785914ba25a7b7c5480c193d7e
dgschel <goesc_@hotmail.de>
2024-08-21 19:25:19 +0200

    basic structure for saving a submission

--- backend/src/functions/saveSurveySubmissionToCosmosDbHttp.ts
+++ backend/src/functions/saveSurveySubmissionToCosmosDbHttp.ts
@@ -10,9 +10,31 @@ const cosmosOutput = output.cosmosDB({
 export async function saveSurveySubmissionToCosmosDbHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    try {
+        const submission: { survey: any, surveyId: string } = await request.json() as any;
 
-    return { body: `Hello, ${name}!` };
+        context.log(`Submission:`, submission);
+
+        // Save the survey data to Cosmos DB
+        context.extraOutputs.set(cosmosOutput, {
+            id: context.invocationId, // Unique ID for the document
+            surveyId: submission.surveyId, // Partition key is the survey
+            submission: submission.survey
+        });
+
+        return { jsonBody: { message: `OK` } };
+
+    } catch (error) {
+        context.log(`Error:`, error);
+
+        return {
+            jsonBody: {
+                message: error.message || "An unexpected error occurred",
+            },
+            status: 500
+        };
+
+    }
 };
 
 app.http('saveSurveySubmissionToCosmosDbHttp', {
