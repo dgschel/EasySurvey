34f50816ccfb44ef9a60d029820db284ebae5d4d
dgschel <goesc_@hotmail.de>
2024-08-21 20:05:21 +0200

    add env endpoints

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   endpoints: {
     saveSurvey: 'http://localhost:7071/api/saveSurveyToCosmosDbHttp',
+    saveSubmission: 'http://localhost:7071/api/saveSurveySubmissionToCosmosDbHttp',
     readSurvey: 'http://localhost:7071/api/readSurveyFromCosmosDbHttp',
   }
 };
