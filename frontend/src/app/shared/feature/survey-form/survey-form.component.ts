1da28292d344538616b80b18592669530f45f2e9
dgschel <goesc_@hotmail.de>
2024-08-21 20:01:39 +0200

    rename variable

--- frontend/src/app/shared/feature/survey-form/survey-form.component.ts
+++ frontend/src/app/shared/feature/survey-form/survey-form.component.ts
@@ -57,15 +57,15 @@ export class SurveyFormComponent implements AfterViewInit {
   }
 
   save() {
-    const models = this.cmpRefs.map((cmpRef) => cmpRef.instance.surveyModel())
+    const surveyModels = this.cmpRefs.map((cmpRef) => cmpRef.instance.surveyModel())
 
-    if (models.length === 0) return;
+    if (surveyModels.length === 0) return;
 
     // Get a *reference* of the radio models and iterate over their names
-    const radioModels = models.filter((model) => model.type === 'radio') as SurveyRadioModel[];
+    const radioModels = surveyModels.filter((model) => model.type === 'radio') as SurveyRadioModel[];
     this.generateRadioNames(radioModels);
 
-    this.azureSurveyService.saveSurveyData(models).subscribe(data => console.log("Data saved", data));
+    this.azureSurveyService.saveSurveyData(surveyModels).subscribe(data => console.log("Data saved", data));
   }
 
   generateRadioNames(models: SurveyRadioModel[]) {
