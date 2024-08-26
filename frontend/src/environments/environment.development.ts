3d3d19998894fe760d650c0be60859a74bcfc5a3
dgschel <goesc_@hotmail.de>
2024-08-26 09:20:09 +0200

    add dev and prod connections string

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -4,5 +4,8 @@ export const environment = {
     saveSurvey: 'http://localhost:7071/api/saveSurveyToCosmosDbHttp',
     saveSubmission: 'http://localhost:7071/api/saveSurveySubmissionToCosmosDbHttp',
     readSurvey: 'http://localhost:7071/api/readSurveyFromCosmosDbHttp',
+  },
+  appInsights: {
+    connectionString: 'InstrumentationKey=306d0c23-ca56-42c8-b50b-dfe5022c7737;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://germanywestcentral.livediagnostics.monitor.azure.com/;ApplicationId=da009b8c-4793-4d39-8d9d-e5a03cd6abba'
   }
 };
