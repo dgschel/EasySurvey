508fa44aac09c4d41ac2312c928bb7f34ea1eabe
dgschel <goesc_@hotmail.de>
2024-08-27 12:03:15 +0200

    receive message and write to blob

--- backend/src/functions/readSurveySubmissionMessage.ts
+++ backend/src/functions/readSurveySubmissionMessage.ts
@@ -1,11 +1,34 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, input, InvocationContext, output } from "@azure/functions";
+
+const blobInput = input.storageBlob({
+    path: 'statistic/{surveyId}.json',
+    connection: 'storageConnection',
+});
+
+const blobOutput = output.storageBlob({
+    path: 'statistic/{surveyId}.json',
+    connection: 'storageConnection',
+})
 
 export async function readSurveySubmissionMessage(queueItem: unknown, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
+
+    const blob = context.extraInputs.get(blobInput)
+
+    if (blob) {
+        // Log the blob
+        context.log('Blob:', blob);
+    }
+    else {
+        // Save the blob to the output
+        context.extraOutputs.set(blobOutput, queueItem)
+    }
 }
 
 app.storageQueue('readSurveySubmissionMessage', {
     queueName: 'survey-submission-messages-001',
     connection: 'storageConnection',
+    extraInputs: [blobInput],
+    extraOutputs: [blobOutput],
     handler: readSurveySubmissionMessage
 });
