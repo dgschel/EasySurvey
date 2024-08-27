b07dee196e4ca59e406114cc054498f65588f1cf
dgschel <goesc_@hotmail.de>
2024-08-27 15:52:14 +0200

    use new types and write data to blob

--- backend/src/functions/readSurveySubmissionMessage.ts
+++ backend/src/functions/readSurveySubmissionMessage.ts
@@ -1,7 +1,9 @@
 import { app, input, InvocationContext, output } from "@azure/functions";
+import { SubmissionStatistic, SurveyStatistic } from "../models/statistic";
+import { SurveySubmissionMessage } from "../models/queue-storage";
 
 const blobInput = input.storageBlob({
-    path: 'statistic/{surveyId}.json',
+    path: 'statistic/{surveyId}.json', // {surveyId} is a key of the json from the parameter queueItem
     connection: 'storageConnection',
 });
 
@@ -10,18 +12,37 @@ const blobOutput = output.storageBlob({
     connection: 'storageConnection',
 })
 
-export async function readSurveySubmissionMessage(queueItem: unknown, context: InvocationContext): Promise<void> {
+export async function readSurveySubmissionMessage(queueItem: SurveySubmissionMessage, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
 
-    const blob = context.extraInputs.get(blobInput)
+    const blob = context.extraInputs.get(blobInput);
 
-    if (blob) {
-        // Log the blob
-        context.log('Blob:', blob);
-    }
-    else {
-        // Save the blob to the output
-        context.extraOutputs.set(blobOutput, queueItem)
+    if (!blob) {
+        // Create blob and save default content to it
+        // it means that this was the first submission for this survey
+        const defaultStatistic: SurveyStatistic = {
+            submissionTotalCount: 1,
+            submissionSuccessCount: queueItem.status === 'success' ? 1 : 0,
+            submissionFailureCount: queueItem.status === 'failure' ? 1 : 0,
+            submissionSuccessRate: queueItem.status === 'success' ? 1 : 0,
+            submissionFailureRate: queueItem.status === 'failure' ? 1 : 0,
+            submissionAverageDurationInMS: 0,
+            submission: {
+                [queueItem.submissionId]: {
+                    date: queueItem.date,
+                    status: queueItem.status
+                }
+            }
+        }
+        context.extraOutputs.set(blobOutput, defaultStatistic)
+    } else {
+        // Read blob and update it with the new submission
+        const blob = context.extraInputs.get(blobInput) as SurveyStatistic;
+        blob.submission[queueItem.submissionId] = {
+            date: queueItem.date,
+            status: queueItem.status
+        };
+        context.extraOutputs.set(blobOutput, blob)
     }
 }
 
