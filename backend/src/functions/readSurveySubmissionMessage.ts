c1a70e47560f509069509b0543405431516ea9a6
dgschel <goesc_@hotmail.de>
2024-08-28 08:18:16 +0200

    remove inital creation of blob, just update with new data

--- backend/src/functions/readSurveySubmissionMessage.ts
+++ backend/src/functions/readSurveySubmissionMessage.ts
@@ -1,5 +1,5 @@
 import { app, input, InvocationContext, output } from "@azure/functions";
-import { SubmissionStatistic, SurveyStatistic } from "../models/statistic";
+import { SurveyStatistic } from "../models/statistic";
 import { SurveySubmissionMessage } from "../models/queue-storage";
 
 const blobInput = input.storageBlob({
@@ -15,35 +15,15 @@ const blobOutput = output.storageBlob({
 export async function readSurveySubmissionMessage(queueItem: SurveySubmissionMessage, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
 
-    const blob = context.extraInputs.get(blobInput);
+    // Read blob and update it with the new submission
+    const blob = context.extraInputs.get(blobInput) as SurveyStatistic;
 
-    if (!blob) {
-        // Create blob and save default content to it
-        // it means that this was the first submission for this survey
-        const defaultStatistic: SurveyStatistic = {
-            submissionTotalCount: 1,
-            submissionSuccessCount: queueItem.status === 'success' ? 1 : 0,
-            submissionFailureCount: queueItem.status === 'failure' ? 1 : 0,
-            submissionSuccessRate: queueItem.status === 'success' ? 1 : 0,
-            submissionFailureRate: queueItem.status === 'failure' ? 1 : 0,
-            submissionAverageDurationInMS: 0,
-            submission: {
-                [queueItem.submissionId]: {
-                    date: queueItem.date,
-                    status: queueItem.status
-                }
-            }
-        }
-        context.extraOutputs.set(blobOutput, defaultStatistic)
-    } else {
-        // Read blob and update it with the new submission
-        const blob = context.extraInputs.get(blobInput) as SurveyStatistic;
-        blob.submission[queueItem.submissionId] = {
-            date: queueItem.date,
-            status: queueItem.status
-        };
-        context.extraOutputs.set(blobOutput, blob)
-    }
+    blob.submission[queueItem.submissionId] = {
+        date: queueItem.date,
+        status: queueItem.status
+    };
+
+    context.extraOutputs.set(blobOutput, blob)
 }
 
 app.storageQueue('readSurveySubmissionMessage', {
