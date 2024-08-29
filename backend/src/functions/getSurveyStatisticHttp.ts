fa17d4e3f513142f0ef611d05929c2e040e08079
dgschel <goesc_@hotmail.de>
2024-08-29 10:47:47 +0200

    map survey statistic from blob with each submission from cosmosdb

--- backend/src/functions/getSurveyStatisticHttp.ts
+++ backend/src/functions/getSurveyStatisticHttp.ts
@@ -3,6 +3,7 @@ import { SurveyStatistic } from "../models/statistic";
 import { Submission } from "../models/submission";
 import { SubmissionSchema } from "../schemas/submission";
 import { SurveyStatisticSchema } from "../schemas/statistic";
+import { SurveyStatisticResponseSchema } from "../schemas/http";
 
 const blobInput = input.storageBlob({
     path: 'statistic/{surveyId}.json', // {surveyId} is a key of the json from the parameter queueItem
@@ -36,10 +37,39 @@ export async function getSurveyStatisticHttp(request: HttpRequest, context: Invo
             throw new Error("Submission data is invalid");
         }
 
+        // Map the submission data to the survey statistic
+        const mappedStatistics = parsedSubmission.data.reduce((acc, curr) => {
+            if (curr.id in parsedSurveyStatistic.data.submission) {
+                return {
+                    ...acc,
+                    [curr.id]: {
+                        submission: curr.submission,
+                        status: curr.status
+                    }
+                };
+            }
+
+            return acc;
+        }, {});
+
+        // Merge the survey statistic data with the replaced submission data
+        const mergedStatistic = {
+            ...parsedSurveyStatistic.data,
+            submission: mappedStatistics
+        }
+
+        // Validate the merged statistic data
+        const parsedMergedStatistic = SurveyStatisticResponseSchema.safeParse(mergedStatistic)
+
+        if (!parsedMergedStatistic.success) {
+            context.log(`Validation errors:`, parsedMergedStatistic.error.errors);
+            throw new Error("Merged statistic data is invalid");
+        }
+
         return {
             jsonBody: {
                 message: `Statistic for survey ${request.params.surveyId}`,
-                data: {}
+                data: parsedMergedStatistic.data
             },
             status: 200
         };
