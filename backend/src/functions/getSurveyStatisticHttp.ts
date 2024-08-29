68bf2f8c97ff3e08517e62bc98edf099119cd37f
dgschel <goesc_@hotmail.de>
2024-08-29 08:30:08 +0200

    parse both bindings and wrap in try-catch

--- backend/src/functions/getSurveyStatisticHttp.ts
+++ backend/src/functions/getSurveyStatisticHttp.ts
@@ -1,5 +1,8 @@
 import { app, HttpRequest, HttpResponseInit, input, InvocationContext } from "@azure/functions";
-import { SubmissionStatistic, SurveyStatistic } from "../models/statistic";
+import { SurveyStatistic } from "../models/statistic";
+import { Submission } from "../models/submission";
+import { SubmissionSchema } from "../schemas/submission";
+import { SurveyStatisticSchema } from "../schemas/statistic";
 
 const blobInput = input.storageBlob({
     path: 'statistic/{surveyId}.json', // {surveyId} is a key of the json from the parameter queueItem
@@ -16,22 +19,39 @@ const cosmosInput = input.cosmosDB({
 export async function getSurveyStatisticHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const surveyStatistic = context.extraInputs.get(blobInput) as SurveyStatistic;
-    const submissions = context.extraInputs.get(cosmosInput) as SubmissionStatistic[];
+    try {
+        const surveyStatistic = context.extraInputs.get(blobInput) as SurveyStatistic;
+        const submissions = context.extraInputs.get(cosmosInput) as Submission[];
 
-    context.log('Blob content:', surveyStatistic);
-    context.log('Cosmos content:', submissions);
+        const parsedSurveyStatistic = SurveyStatisticSchema.safeParse(surveyStatistic);
+        const parsedSubmission = SubmissionSchema.pick({ id: true, submission: true, status: true }).array().safeParse(submissions);
 
-    return {
-        jsonBody: {
-            message: `Statistic for survey ${request.params.surveyId}`,
-            data: {
-                [request.params.surveyId]: surveyStatistic,
-                submissions
-            }
-        },
-        status: 200
-    };
+        if (!parsedSurveyStatistic.success) {
+            context.log(`Validation errors:`, parsedSurveyStatistic.error.errors);
+            throw new Error("Survey statistic data is invalid");
+        }
+
+        if (!parsedSubmission.success) {
+            context.log(`Validation errors:`, parsedSubmission.error.errors);
+            throw new Error("Submission data is invalid");
+        }
+
+        return {
+            jsonBody: {
+                message: `Statistic for survey ${request.params.surveyId}`,
+                data: {}
+            },
+            status: 200
+        };
+    } catch (error) {
+
+        return {
+            jsonBody: {
+                message: error.message || "An unexpected error occurred",
+            },
+            status: 500
+        };
+    }
 };
 
 app.http('getSurveyStatisticHttp', {
