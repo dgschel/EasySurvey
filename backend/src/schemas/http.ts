92ea5886609ee48919b823e1563a2a368d0db43f
dgschel <goesc_@hotmail.de>
2024-08-30 14:54:16 +0200

    fix schema

--- backend/src/schemas/http.ts
+++ backend/src/schemas/http.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { SurveyStatisticSchema } from "./statistic";
 
 export const SubmissionCountSchema = z.record(z.string(), z.number());
-export const SubmissionInputCountSchema = z.record(z.string(), z.string().array());
+export const SubmissionInputCountSchema = z.array(z.string());
 export const SubmissionResponseSchema = z.union([SubmissionCountSchema, SubmissionInputCountSchema]);
 
 export const SurveyStatisticResponseSchema = SurveyStatisticSchema.extend({
