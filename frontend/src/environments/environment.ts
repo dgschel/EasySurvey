43a001541a0df84becb209fd444d175822d44c9e
dgschel <goesc_@hotmail.de>
2024-09-02 11:23:31 +0200

    update env variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
     saveSurvey: 'https://localhost:7071/api/saveSurveyToCosmosDbHttp',
     saveSubmission: 'http://localhost:7071/api/saveSurveySubmissionToCosmosDbHttp',
     readSurvey: 'https://localhost:7071/api/readSurveyFromCosmosDbHttp',
+    getSurveyStatistic: 'http://localhost:7071/api/survey/{surveyId}/statistic',
   },
   appInsights: {
     connectionString: 'InstrumentationKey=9d844b42-2de9-41e2-9458-6709302cbb32;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://germanywestcentral.livediagnostics.monitor.azure.com/;ApplicationId=e330c31a-bc19-439e-93f3-2ad0efdc5261'
