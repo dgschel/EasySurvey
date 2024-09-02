29ac38debc5b7d45b5d998c7760143d4da20e5a6
dgschel <goesc_@hotmail.de>
2024-09-02 11:06:12 +0200

    add survey statistic type

--- frontend/src/app/util/type/statistic.ts
+++ frontend/src/app/util/type/statistic.ts
@@ -3,3 +3,17 @@ export type SubmissionStatistic = {
   endDate: Date;
   status: 'success' | 'failure';
 };
+
+/**
+ * Survey statistic data
+ * @path backend\src\schemas\http.ts
+ */
+export type SurveyStatisticResponse = {
+  submissionTotalCount: number;
+  submissionSuccessCount: number;
+  submissionFailureCount: number;
+  submissionSuccessRate: number;
+  submissionFailureRate: number;
+  submissionAverageDurationInMS: number;
+  submission: Record<string, Record<string, number> | string[]>
+}
