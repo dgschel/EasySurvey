cf2a1d31bd1f50c1e06e3698f1ce731202e1747d
dgschel <goesc_@hotmail.de>
2024-09-02 09:27:30 +0200

    add new route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ViewSurveyFormComponent } from './view-survey-form/view-survey-form.component';
 import { StoreFormComponent } from './store-form/store-form.component';
+import { StatisticComponent } from './statistic/statistic.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home/:template', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'store', component: StoreFormComponent },
+  { path: 'statistic', component: StatisticComponent },
   { path: ':id/viewform', component: ViewSurveyFormComponent },
   { path: '**', redirectTo: '/home' }
 ];
