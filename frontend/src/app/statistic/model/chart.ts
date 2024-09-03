10bafbfcfe2e7d479967c3e29eb4393ba2fc0f24
dgschel <goesc_@hotmail.de>
2024-09-03 14:16:44 +0200

    add grid config

--- frontend/src/app/statistic/model/chart.ts
+++ frontend/src/app/statistic/model/chart.ts
@@ -1,11 +1,11 @@
-import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexDataLabels, ApexPlotOptions, ApexYAxis, ApexTooltip, ApexTitleSubtitle, ApexLegend, ApexGrid, ApexFill } from "ng-apexcharts";
+import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexDataLabels, ApexPlotOptions, ApexYAxis, ApexTooltip, ApexTitleSubtitle, ApexLegend, ApexGrid } from "ng-apexcharts";
 
 export type ChartOption = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   dataLabels: ApexDataLabels;
   plotOptions: ApexPlotOptions;
-  grid: ApexGrid,
+  grid: ApexGrid;
   legend: ApexLegend;
   yaxis: ApexYAxis;
   xaxis: ApexXAxis;
