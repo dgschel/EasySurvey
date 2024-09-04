4db44b870f9e2d8cd632f9a7df218f25d9251bf8
dgschel <goesc_@hotmail.de>
2024-09-04 13:05:35 +0200

    updated colors

--- frontend/src/app/statistic/statistic.component.ts
+++ frontend/src/app/statistic/statistic.component.ts
@@ -78,6 +78,7 @@ export class StatisticComponent implements OnInit {
         }
       },
       series,
+      colors: ["#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e", "#ebdc78", "#ff9e00"],
       grid: {
         show: false,
         padding: {
