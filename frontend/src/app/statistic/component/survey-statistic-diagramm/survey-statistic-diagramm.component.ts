c36752f04ec9ed9696ceb472dfd8b0f184637cf6
dgschel <goesc_@hotmail.de>
2024-09-04 08:51:01 +0200

    listen and dispose for resize event

--- frontend/src/app/statistic/component/survey-statistic-diagramm/survey-statistic-diagramm.component.ts
+++ frontend/src/app/statistic/component/survey-statistic-diagramm/survey-statistic-diagramm.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { debounceTime, fromEvent, Subscription } from 'rxjs';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { ChartOption } from '../../model/chart';
 
@@ -9,6 +10,21 @@ import { ChartOption } from '../../model/chart';
   templateUrl: './survey-statistic-diagramm.component.html',
   styleUrl: './survey-statistic-diagramm.component.scss'
 })
-export class SurveyStatisticDiagrammComponent {
+export class SurveyStatisticDiagrammComponent implements AfterViewInit, OnDestroy {
+  @ViewChild("chart") chart!: ElementRef<HTMLDivElement>;
   @Input() chartOption: Partial<ChartOption> = {}
+  resizeSubscription: Subscription = new Subscription();
+
+  ngAfterViewInit(): void {
+    // Set the width of the chart to the width of the window minus the padding of the parent element
+    // This is necessary because the chart does not automatically adjust its width when the window is resized
+    // We need this code since if more than one chart is displayed, the automatic width adjustment does not work
+    this.resizeSubscription = fromEvent(window, 'resize').pipe(debounceTime(500)).subscribe(() => {
+      this.chart.nativeElement.style.width = window.innerWidth - 52 + 'px';
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.resizeSubscription.unsubscribe();
+  }
 }
