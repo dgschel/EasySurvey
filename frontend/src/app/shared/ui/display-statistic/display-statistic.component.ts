c27c55c4b368bcffd13f69b7d32cb38d8943476b
dgschel <goesc_@hotmail.de>
2024-09-05 11:04:28 +0200

    created component to display statistics

---
+++ frontend/src/app/shared/ui/display-statistic/display-statistic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-statistic',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-statistic.component.html',
+  styleUrl: './display-statistic.component.scss'
+})
+export class DisplayStatisticComponent {
+
+}
