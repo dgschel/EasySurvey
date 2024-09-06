5b43b973d397935faaa25489e2a83e2e48ae4a44
dgschel <goesc_@hotmail.de>
2024-09-06 12:08:44 +0200

    add imports

--- frontend/src/app/statistic/component/table-statistic/table-statistic.component.ts
+++ frontend/src/app/statistic/component/table-statistic/table-statistic.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SubmissionInputCount } from '../../../util/type/statistic';
 
 @Component({
   selector: 'app-table-statistic',
   standalone: true,
-  imports: [],
+  imports: [KeyValuePipe],
   templateUrl: './table-statistic.component.html',
   styleUrl: './table-statistic.component.scss'
 })
 export class TableStatisticComponent {
-
+  @Input('statistic') statistic: Record<string, SubmissionInputCount> = {};
 }
