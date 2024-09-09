45fb399d5b654e0759001df18088fc4002ddb1b4
dgschel <goesc_@hotmail.de>
2024-09-09 11:16:30 +0200

    add base api url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
+  apiUrl: 'http://localhost:7071/api',
   endpoints: {
-    saveSurvey: 'http://localhost:7071/api/saveSurveyToCosmosDbHttp',
-    saveSubmission: 'http://localhost:7071/api/saveSurveySubmissionToCosmosDbHttp',
-    readSurvey: 'http://localhost:7071/api/readSurveyFromCosmosDbHttp',
-    getSurveyStatistic: 'http://localhost:7071/api/survey/{surveyId}/statistic',
-    createQRCode: 'http://localhost:7071/api/createQRCodeHttp',
+    saveSurvey: 'saveSurveyToCosmosDbHttp',
+    saveSubmission: 'saveSurveySubmissionToCosmosDbHttp',
+    readSurvey: 'readSurveyFromCosmosDbHttp',
+    getSurveyStatistic: 'survey/{surveyId}/statistic',
+    createQRCode: 'createQRCodeHttp',
   },
   appInsights: {
     connectionString: 'InstrumentationKey=306d0c23-ca56-42c8-b50b-dfe5022c7737;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://germanywestcentral.livediagnostics.monitor.azure.com/;ApplicationId=da009b8c-4793-4d39-8d9d-e5a03cd6abba'
