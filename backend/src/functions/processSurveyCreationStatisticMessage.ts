564a2aeacd3f66a3e6b01a7a531619e2d45d0ae1
dgschel <goesc_@hotmail.de>
2024-09-10 13:53:56 +0200

    remove input and instead use only output binding

--- backend/src/functions/processSurveyCreationStatisticMessage.ts
+++ backend/src/functions/processSurveyCreationStatisticMessage.ts
@@ -1,9 +1,4 @@
-import { app, InvocationContext, input, output } from "@azure/functions";
-
-const blobInput = input.storageBlob({
-    connection: 'storageConnection',
-    path: 'statistic/{surveyId}.json',
-});
+import { app, InvocationContext, output } from "@azure/functions";
 
 const blobOutput = output.storageBlob({
     path: 'statistic/{surveyId}.json',
@@ -13,14 +8,13 @@ const blobOutput = output.storageBlob({
 export async function processSurveyCreationStatisticMessage(queueItem: unknown, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
 
-    const blob = context.extraInputs.get(blobInput);
-    context.log('Blob content:', blob);
+    // Read blob and create an initial statistic file
+    const blob = context.extraOutputs.get(blobOutput);
 }
 
 app.storageQueue('processSurveyCreationStatisticMessage', {
     queueName: 'survey-statistic-message-001',
     connection: 'storageConnection',
-    extraInputs: [blobInput],
     extraOutputs: [blobOutput],
     handler: processSurveyCreationStatisticMessage
 });
