035fa84d5cc8891ff4c73c100b5e511f8100f933
dgschel <goesc_@hotmail.de>
2024-09-10 13:49:11 +0200

    use inputBinding to read from blobstorage

--- backend/src/functions/processSurveyCreationStatisticMessage.ts
+++ backend/src/functions/processSurveyCreationStatisticMessage.ts
@@ -1,4 +1,9 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, InvocationContext, input } from "@azure/functions";
+
+const blobInput = input.storageBlob({
+    connection: 'storageConnection',
+    path: 'statistic/{surveyId}.json',
+});
 
 export async function processSurveyCreationStatisticMessage(queueItem: unknown, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
@@ -7,5 +12,6 @@ export async function processSurveyCreationStatisticMessage(queueItem: unknown, 
 app.storageQueue('processSurveyCreationStatisticMessage', {
     queueName: 'survey-statistic-message-001',
     connection: 'storageConnection',
+    extraInputs: [blobInput],
     handler: processSurveyCreationStatisticMessage
 });
