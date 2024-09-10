02bf20182eab781baadf810786f4eebc4518d517
dgschel <goesc_@hotmail.de>
2024-09-10 13:52:37 +0200

    create an output to write back to file

--- backend/src/functions/processSurveyCreationStatisticMessage.ts
+++ backend/src/functions/processSurveyCreationStatisticMessage.ts
@@ -1,17 +1,26 @@
-import { app, InvocationContext, input } from "@azure/functions";
+import { app, InvocationContext, input, output } from "@azure/functions";
 
 const blobInput = input.storageBlob({
     connection: 'storageConnection',
     path: 'statistic/{surveyId}.json',
 });
 
+const blobOutput = output.storageBlob({
+    path: 'statistic/{surveyId}.json',
+    connection: 'storageConnection',
+})
+
 export async function processSurveyCreationStatisticMessage(queueItem: unknown, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
+
+    const blob = context.extraInputs.get(blobInput);
+    context.log('Blob content:', blob);
 }
 
 app.storageQueue('processSurveyCreationStatisticMessage', {
     queueName: 'survey-statistic-message-001',
     connection: 'storageConnection',
     extraInputs: [blobInput],
+    extraOutputs: [blobOutput],
     handler: processSurveyCreationStatisticMessage
 });
