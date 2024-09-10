983314e62051b499a1fc7b550fac532cfe1279dc
dgschel <goesc_@hotmail.de>
2024-09-10 14:00:43 +0200

    create file based on surveyId

--- backend/src/functions/processSurveyCreationStatisticMessage.ts
+++ backend/src/functions/processSurveyCreationStatisticMessage.ts
@@ -1,15 +1,27 @@
 import { app, InvocationContext, output } from "@azure/functions";
+import { SurveyStatistic } from "../models/statistic";
 
 const blobOutput = output.storageBlob({
     path: 'statistic/{surveyId}.json',
     connection: 'storageConnection',
 })
 
-export async function processSurveyCreationStatisticMessage(queueItem: unknown, context: InvocationContext): Promise<void> {
-    context.log('Storage queue function processed work item:', queueItem);
+export async function processSurveyCreationStatisticMessage(queueItem: { surveyId: string }, context: InvocationContext): Promise<void> {
+    context.log('Storage queue function processed work item:', queueItem.surveyId);
 
-    // Read blob and create an initial statistic file
-    const blob = context.extraOutputs.get(blobOutput);
+    // Create an initial statistic file
+    const initialStatistic: SurveyStatistic = {
+        submissionTotalCount: 0,
+        submissionSuccessCount: 0,
+        submissionFailureCount: 0,
+        submissionSuccessRate: 0,
+        submissionFailureRate: 0,
+        submissionAverageDurationInMS: 0,
+        submission: {}
+    }
+
+    // Save the initial statistic to the blob storage with the surveyId as the filename
+    context.extraOutputs.set(blobOutput, initialStatistic);
 }
 
 app.storageQueue('processSurveyCreationStatisticMessage', {
