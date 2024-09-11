0e1631c84972f996525d6c36dc444eef128e264f
dgschel <goesc_@hotmail.de>
2024-09-11 11:41:14 +0200

    load svg to display

--- frontend/src/app/shared/ui/template/modal/survey-successfully-saved/survey-successfully-saved.component.ts
+++ frontend/src/app/shared/ui/template/modal/survey-successfully-saved/survey-successfully-saved.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { SvgIconComponent, SvgIconRegistryService } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-survey-successfully-saved',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './survey-successfully-saved.component.html',
   styleUrl: './survey-successfully-saved.component.scss'
 })
-export class SurveySuccessfullySavedComponent {
+export class SurveySuccessfullySavedComponent implements OnInit, OnDestroy {
+  private iconReg = inject(SvgIconRegistryService);
 
+  ngOnInit(): void {
+    this.iconReg.loadSvg('/svg/rosette-discount-check.svg', 'rosette-discount-check')?.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.iconReg.unloadSvg('rosette-discount-check');
+  }
 }
