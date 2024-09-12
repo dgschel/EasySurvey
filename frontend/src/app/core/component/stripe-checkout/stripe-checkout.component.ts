3e68ec7b95a8e6c57125705124442a746e0d5230
dgschel <goesc_@hotmail.de>
2024-09-12 16:16:18 +0200

    declarative and reactive embedded stripe form

--- frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
+++ frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
@@ -1,18 +1,32 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { EMPTY, filter, from, switchMap, map, Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 import { StripeCheckoutService } from '../../service/stripe-checkout.service';
 
 @Component({
   selector: 'app-stripe-checkout',
   standalone: true,
-  imports: [],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './stripe-checkout.component.html',
   styleUrl: './stripe-checkout.component.scss'
 })
 export class StripeCheckoutComponent implements OnInit {
   private stripeService = inject(StripeCheckoutService);
+  private route = inject(ActivatedRoute);
+
+  @ViewChild('checkout') stripeCheckoutElement!: ElementRef<HTMLDivElement>;
+
+  checkout$: Observable<void> = EMPTY;
 
   ngOnInit() {
-    const stripe = this.stripeService.getStripeInstance();
+    this.checkout$ = this.route.paramMap.pipe(
+      filter(params => params.has('id')),
+      map(params => params.get('id') as string),
+      switchMap(surveyId => this.stripeService.fetchClientSecret(surveyId)),
+      switchMap(({ data }) => from(this.stripeService.initEmbeddedCheckout(data.clientSecret))),
+      map(checkout => checkout.mount(this.stripeCheckoutElement.nativeElement))
+    )
   }
 }
