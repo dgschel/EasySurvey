e3dc140dfc60cea246fb71f57c26aab2e30efdad
dgschel <goesc_@hotmail.de>
2024-09-12 10:32:42 +0200

    init stripe with secret and price id

--- backend/src/functions/stripeCheckoutHttp.ts
+++ backend/src/functions/stripeCheckoutHttp.ts
@@ -1,15 +1,43 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import Stripe from 'stripe';
 
 export async function stripeCheckoutHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    try {
+        const stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY);
+        const session = await stripe.checkout.sessions.create({
+            line_items: [{
+                price: process.env.STRIPE_PRICE_ID,
+                quantity: 1,
+            }],
+            mode: 'payment',
+            ui_mode: 'embedded',
+            return_url: `${request.headers.get('origin')}/checkout/return?session_id={CHECKOUT_SESSION_ID}'`
+        });
 
-    return { body: `Hello, ${name}!` };
+        return {
+            jsonBody: {
+                message: `Stripe Checkout session created successfully`,
+                data: {
+                    clientSecret: session.client_secret
+                }
+            }
+        }
+
+    } catch (error) {
+        return {
+            jsonBody: {
+                message: `Failed to create Stripe Checkout session`,
+                error: error.message || "An unexpected error occurred"
+            },
+            status: 500
+        };
+    }
 };
 
 app.http('stripeCheckoutHttp', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
+    methods: ['GET'],
+    authLevel: 'function',
     handler: stripeCheckoutHttp
 });
