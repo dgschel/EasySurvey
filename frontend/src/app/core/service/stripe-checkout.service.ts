a89f3471912dd415105ca5c3710910dbb568623c
dgschel <goesc_@hotmail.de>
2024-09-12 16:03:51 +0200

    improve service by add additional functions

--- frontend/src/app/core/service/stripe-checkout.service.ts
+++ frontend/src/app/core/service/stripe-checkout.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@angular/core';
-import { loadStripe, Stripe } from '@stripe/stripe-js';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { loadStripe, Stripe, StripeEmbeddedCheckout } from '@stripe/stripe-js';
+
 import { environment } from '../../../environments/environment.development';
+import { HttpService } from './http.service';
+import { HttpWrapper } from '../../util/type/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StripeCheckoutService {
+  private httpService = inject(HttpService);
   private stripe: Stripe | null = null;
 
   constructor() {
@@ -20,5 +25,14 @@ export class StripeCheckoutService {
 
   getStripeInstance(): Stripe | null {
     return this.stripe
+  }
+
+  fetchClientSecret(surveyId: string): Observable<HttpWrapper<{ clientSecret: string; sessionId: string; }>> {
+    return this.httpService.post<{ clientSecret: string, sessionId: string }>(environment.endpoints.stripeCheckout, { id: surveyId });
+  }
+
+  async initEmbeddedCheckout(clientSecret: string): Promise<StripeEmbeddedCheckout> {
+    if (!this.stripe) return Promise.reject('Stripe is not initialized');
+    return await this.stripe.initEmbeddedCheckout({ clientSecret })
   }
 }
