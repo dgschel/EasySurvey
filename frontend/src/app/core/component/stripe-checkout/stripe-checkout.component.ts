5df3d097377f08d31c0c4f2c2af01433732f6b9f
dgschel <goesc_@hotmail.de>
2024-09-12 10:42:14 +0200

    add comp

---
+++ frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-checkout',
+  standalone: true,
+  imports: [],
+  templateUrl: './stripe-checkout.component.html',
+  styleUrl: './stripe-checkout.component.scss'
+})
+export class StripeCheckoutComponent {
+
+}
