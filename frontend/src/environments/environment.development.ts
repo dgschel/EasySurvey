1c2e84c3af4809e8c64f21640edd16ad380ec24f
dgschel <goesc_@hotmail.de>
2024-09-12 12:08:33 +0200

    add stripe public key

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -7,6 +7,10 @@ export const environment = {
     readSurvey: 'readSurveyFromCosmosDbHttp',
     getSurveyStatistic: 'survey/{surveyId}/statistic',
     createQRCode: 'createQRCodeHttp',
+    stripeCheckout: 'createStripeCheckoutSessionHttp'
+  },
+  stripe: {
+    publicKey: 'pk_test_51H2cteFqohewLde01E4dKVd9Vtv3r7Cqcwo3OBF2j1gICzkOiGdSwn893njCErZtd8cwUwEonqyVHCfWGEENoUTx00rjpT4g2T'
   },
   appInsights: {
     connectionString: 'InstrumentationKey=306d0c23-ca56-42c8-b50b-dfe5022c7737;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://germanywestcentral.livediagnostics.monitor.azure.com/;ApplicationId=da009b8c-4793-4d39-8d9d-e5a03cd6abba'
