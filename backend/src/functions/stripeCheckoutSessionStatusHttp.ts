c001fd00f4a645d2045e2d4fcb56c8c9d0483632
dgschel <goesc_@hotmail.de>
2024-09-13 17:28:29 +0200

    fetch session from stripe checkout

--- backend/src/functions/stripeCheckoutSessionStatusHttp.ts
+++ backend/src/functions/stripeCheckoutSessionStatusHttp.ts
@@ -1,15 +1,36 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import Stripe from 'stripe';
 
 export async function stripeCheckoutSessionStatusHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    try {
+        const stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY);
+        const session = await stripe.checkout.sessions.retrieve(request.query.get('session_id'));
 
-    return { body: `Hello, ${name}!` };
+        context.log(`Stripe Checkout session retrieved successfully`, session);
+
+        return {
+            jsonBody: {
+                message: `Stripe Checkout session retrieved successfully`,
+                data: { status: session.status, customer_email: session.customer_details.email }
+            }
+        }
+
+    } catch (error) {
+        return {
+            jsonBody: {
+                message: `Failed to fetch Stripe Checkout session`,
+                error: error.message || "An unexpected error occurred"
+            },
+            status: 500
+        };
+
+    }
 };
 
 app.http('stripeCheckoutSessionStatusHttp', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
+    methods: ['GET'],
+    authLevel: 'function',
     handler: stripeCheckoutSessionStatusHttp
 });
