5865984d53ea765b6a4de1ac4a12219a686f7700
dgschel <goesc_@hotmail.de>
2024-09-13 20:45:00 +0200

    fetch checkout-session from backend reading stripe

--- frontend/src/app/core/component/stripe-checkout-session/stripe-checkout-session.component.ts
+++ frontend/src/app/core/component/stripe-checkout-session/stripe-checkout-session.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HttpService } from '../../service/http.service';
+import { environment } from '../../../../environments/environment';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-stripe-checkout-session',
@@ -7,6 +12,16 @@ import { Component } from '@angular/core';
   templateUrl: './stripe-checkout-session.component.html',
   styleUrl: './stripe-checkout-session.component.scss'
 })
-export class StripeCheckoutSessionComponent {
+export class StripeCheckoutSessionComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private httpService = inject(HttpService);
 
+  ngOnInit(): void {
+    this.route.queryParams.pipe(
+      map(params => params['session_id']),
+      switchMap((session_id: string) => this.httpService.get<{ status: string, customerEmail: string, surveyId: string }>(environment.endpoints.stripeCheckoutSessionStatus, { session_id }))
+    ).subscribe(response => {
+      console.log('Stripe checkout session status:', response);
+    })
+  }
 }
