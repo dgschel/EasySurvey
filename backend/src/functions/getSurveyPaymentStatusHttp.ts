26b16e830ec8c913328863a813a9e8d69f7e08ae
dgschel <goesc_@hotmail.de>
2024-09-14 14:58:40 +0200

    fetch query param and validate schema from surveys. return the status of the survey with given id

--- backend/src/functions/getSurveyPaymentStatusHttp.ts
+++ backend/src/functions/getSurveyPaymentStatusHttp.ts
@@ -12,14 +12,38 @@ export async function getSurveyPaymentStatusHttp(request: HttpRequest, context: 
     context.log(`Http function processed request for url "${request.url}"`);
 
     try {
-        const payload = await request.json();
+        const surveyId = request.query.get('surveyId');
 
-        // Validate the payload
-        const parsedPayload = SurveyCosmosDbSchema.pick({ id: true }).safeParse(payload);
+        // Check if there is a survey id
+        if (!surveyId || surveyId === "") {
+            context.log("Survey id is required");
+            throw new Error("Survey id is required");
+        }
 
-        if (!parsedPayload.success) {
-            context.log("Validation Error", parsedPayload.error.errors);
-            throw new Error("Payload is invalid");
+        const surveys = context.extraInputs.get(surveyInput);
+
+        // Check if there is a survey
+        if (Array.isArray(surveys) && surveys.length === 0) {
+            context.log(`Survey not found for id ${request.query.get('surveyId')}`);
+            throw new Error(`Survey not found for id ${request.query.get('surveyId')}`);
+        }
+
+        // Validate the survey data using zod schema definition
+        const parsedSurveys = SurveyCosmosDbSchema.array().safeParse(surveys)
+
+        if (!parsedSurveys.success) {
+            context.log(`Validation errors:`, parsedSurveys.error.errors);
+            throw new Error("Survey data is invalid");
+        }
+
+        // Get the survey
+        const survey = parsedSurveys.data.at(0);
+
+        return {
+            jsonBody: {
+                message: `Survey payment status retrieved successfully`,
+                data: { status: survey.status }
+            }
         }
 
     } catch (error) {
