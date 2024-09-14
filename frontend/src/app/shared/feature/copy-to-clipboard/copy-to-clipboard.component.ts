5538e5d543e1835a3b2aa63632376a2b444a792b
dgschel <goesc_@hotmail.de>
2024-09-14 06:34:40 +0200

    add svg, load and dispose ressource

--- frontend/src/app/shared/feature/copy-to-clipboard/copy-to-clipboard.component.ts
+++ frontend/src/app/shared/feature/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { SvgIconComponent, SvgIconRegistryService } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-copy-to-clipboard',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './copy-to-clipboard.component.html',
   styleUrl: './copy-to-clipboard.component.scss'
 })
-export class CopyToClipboardComponent {
+export class CopyToClipboardComponent implements OnInit {
+  private iconReg = inject(SvgIconRegistryService);
 
+  @Input({ required: true }) textToCopy: string = '';
+
+  ngOnInit(): void {
+    this.iconReg.loadSvg('/svg/clipboard.svg', 'clipboard')?.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.iconReg.unloadSvg('clipboard');
+  }
 }
