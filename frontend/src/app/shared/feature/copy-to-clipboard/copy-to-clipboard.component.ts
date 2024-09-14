3026b8696e92bd2279ace18735e5cbda91c875ce
dgschel <goesc_@hotmail.de>
2024-09-14 06:20:28 +0200

    create comp

---
+++ frontend/src/app/shared/feature/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-copy-to-clipboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './copy-to-clipboard.component.html',
+  styleUrl: './copy-to-clipboard.component.scss'
+})
+export class CopyToClipboardComponent {
+
+}
