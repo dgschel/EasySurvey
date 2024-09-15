314f31e224cfeb0ac0e3a79900c89cbe9ade3c99
dgschel <goesc_@hotmail.de>
2024-09-15 16:15:14 +0200

    change comp to load after route hit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { ViewSurveyFormComponent } from './view-survey-form/view-survey-form.component';
 import { StoreFormComponent } from './store-form/store-form.component';
 import { StatisticComponent } from './statistic/statistic.component';
-import { StripeCheckoutComponent } from './core/component/stripe-checkout/stripe-checkout.component';
 import { StripeCheckoutSessionComponent } from './core/component/stripe-checkout-session/stripe-checkout-session.component';
+import { SurveyCheckoutComponent } from './survey-checkout/survey-checkout.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -13,7 +13,7 @@ export const routes: Routes = [
   { path: 'store', component: StoreFormComponent },
   { path: 'survey/:id/statistic', component: StatisticComponent },
   { path: 'survey/:id/viewform', component: ViewSurveyFormComponent },
-  { path: 'survey/:id/checkout', component: StripeCheckoutComponent },
+  { path: 'survey/:id/checkout', component: SurveyCheckoutComponent },
   { path: 'checkout/return', component: StripeCheckoutSessionComponent },
   { path: '**', redirectTo: '/home' }
 ];
