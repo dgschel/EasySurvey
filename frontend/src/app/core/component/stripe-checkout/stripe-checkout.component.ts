6534af63a5517b4f7b9d4db94dc93b08fb59694a
dgschel <goesc_@hotmail.de>
2024-09-15 17:13:57 +0200

    use input to focus only on init embedded form

--- frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
+++ frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AsyncPipe, NgIf } from '@angular/common';
 
@@ -7,81 +7,35 @@ import { EMPTY, from, switchMap, map, Observable, catchError, withLatestFrom, of
 import { StripeCheckoutService } from '../../service/stripe-checkout.service';
 import { HttpService } from '../../service/http.service';
 import { environment } from '../../../../environments/environment';
+import { SurveyCheckoutComponent } from '../../../survey-checkout/survey-checkout.component';
 
 @Component({
   selector: 'app-stripe-checkout',
   standalone: true,
-  imports: [NgIf, AsyncPipe],
+  imports: [SurveyCheckoutComponent, NgIf, AsyncPipe],
   templateUrl: './stripe-checkout.component.html',
   styleUrl: './stripe-checkout.component.scss'
 })
 export class StripeCheckoutComponent implements OnInit {
-  private httpService = inject(HttpService);
+  @Input('surveyId') surveyId: string = '';
   private stripeService = inject(StripeCheckoutService);
-  private route = inject(ActivatedRoute);
-
-  errorMessage: string = '';
-
   @ViewChild('checkout') stripeCheckoutElement!: ElementRef<HTMLDivElement>;
 
   checkout$: Observable<void> = EMPTY;
+  errorMessage: string = '';
 
   ngOnInit() {
-    // Fetch the survey id from the route and ensure it is valid
-    const surveyId$ = this.route.paramMap.pipe(
-      map(params => params.get('id')),
-      filter((id): id is string => !!id), // Proceed only if surveyId is not null or empty
+    // Fetch client secret for Stripe Checkout
+    const fetchClientSecretObservable$ = this.stripeService.fetchClientSecret(this.surveyId).pipe(
       catchError(error => {
-        this.errorMessage = "Error fetching survey ID from route";
-        console.error("Error fetching survey ID from route:", error);
-        return EMPTY; // Stop further processing if surveyId is invalid
+        this.errorMessage = "Error fetching client secret";
+        console.error("Error fetching client secret:", error);
+        return EMPTY; // Stop further processing if client secret cannot be fetched
       })
     );
 
-    // Fetch the payment status of the survey
-    const surveyPaymentStatus$ = surveyId$.pipe(
-      switchMap(surveyId => {
-        const url = environment.endpoints.surveyPaymentStatus.replace('{surveyId}', surveyId);
-        return this.httpService.get<{ status: string }>(url)
-          .pipe(
-            map(response => response?.data?.status || 'unknown'), // Ensure we have a valid status
-            catchError(error => {
-              this.errorMessage = "Survey not found or has been archived";
-              console.error("Survey not found or has been archived:", error);
-              return of('Survey not found or has been archived'); // Return a fallback value
-            })
-          )
-      })
-    );
-
-    // Branch logic based on payment status
-    const surveyStatusHandling$ = surveyPaymentStatus$.pipe(
-      switchMap(status => {
-        if (status === 'not paid') {
-          return of(status); // Proceed with Stripe checkout flow
-        } else if (status === 'paid') {
-          this.errorMessage = "This survey has already been paid. Redirecting...";
-          // Handle redirect or display additional message, return EMPTY to stop further processing
-          return EMPTY;
-        } else {
-          this.errorMessage = "Unexpected survey payment status.";
-          return EMPTY; // Stop further processing for any unexpected status
-        }
-      })
-    );
-
-    // Fetch the client secret and initialize the Stripe Checkout if survey status is 'not paid'
-    this.checkout$ = surveyStatusHandling$.pipe(
-      withLatestFrom(surveyId$), // Combine with surveyId to proceed
-      switchMap(([_, surveyId]) =>
-        this.stripeService.fetchClientSecret(surveyId).pipe(
-          catchError(error => {
-            this.errorMessage = "Error fetching client secret";
-            console.error("Error fetching client secret:", error);
-            return EMPTY; // Stop further processing if client secret cannot be fetched
-          })
-        )
-      ),
+    // Initialize Stripe Checkout
+    const initializedStripeCheckout$ = fetchClientSecretObservable$.pipe(
       switchMap(({ data }) => {
         if (!data || !data.clientSecret) {
           this.errorMessage = "Invalid client secret for Stripe Checkout";
@@ -95,7 +49,11 @@ export class StripeCheckoutComponent implements OnInit {
             return EMPTY; // Stop further processing if Stripe Checkout initialization fails
           })
         );
-      }),
+      })
+    );
+
+    // Mount Stripe Checkout form
+    this.checkout$ = initializedStripeCheckout$.pipe(
       map(checkout => {
         if (!checkout) {
           this.errorMessage = "Stripe checkout could not be initialized";
