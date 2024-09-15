307e7ffd827ca388414eec1401e034cbcd89285a
dgschel <goesc_@hotmail.de>
2024-09-15 17:23:57 +0200

    receive error message as input and display it

--- frontend/src/app/shared/ui/display-error-message/display-error-message.component.ts
+++ frontend/src/app/shared/ui/display-error-message/display-error-message.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-display-error-message',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './display-error-message.component.scss'
 })
 export class DisplayErrorMessageComponent {
-
+  @Input('errorMessage') errorMessage: string = '';
 }
