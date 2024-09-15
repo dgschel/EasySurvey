0e5210c5d538a0790b7f3334a78b24c616254e1a
dgschel <goesc_@hotmail.de>
2024-09-15 16:14:58 +0200

    add comp

---
+++ frontend/src/app/survey-checkout/survey-checkout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-checkout',
+  standalone: true,
+  imports: [],
+  templateUrl: './survey-checkout.component.html',
+  styleUrl: './survey-checkout.component.scss'
+})
+export class SurveyCheckoutComponent {
+
+}
