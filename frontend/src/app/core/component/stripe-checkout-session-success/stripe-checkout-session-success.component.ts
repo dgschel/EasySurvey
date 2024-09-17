1686a0b3dd33188c6fd072224fd02a979d436b95
dgschel <goesc_@hotmail.de>
2024-09-17 11:34:35 +0200

    fetch qrcode to display

--- frontend/src/app/core/component/stripe-checkout-session-success/stripe-checkout-session-success.component.ts
+++ frontend/src/app/core/component/stripe-checkout-session-success/stripe-checkout-session-success.component.ts
@@ -1,27 +1,44 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { SvgIconComponent, SvgIconRegistryService } from 'angular-svg-icon';
 
 import { StripeCheckoutSessionStatus } from '../../../util/type/stripe';
 import { BasicCardComponent } from '../../../shared/ui/basic-card/basic-card.component';
 import { DisplayQrCodeComponent } from '../../../shared/ui/display-qr-code/display-qr-code.component';
+import { HttpService } from '../../service/http.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-stripe-checkout-session-success',
   standalone: true,
-  imports: [BasicCardComponent, DisplayQrCodeComponent, SvgIconComponent],
+  imports: [BasicCardComponent, DisplayQrCodeComponent, SvgIconComponent, AsyncPipe, RouterLink],
   templateUrl: './stripe-checkout-session-success.component.html',
   styleUrl: './stripe-checkout-session-success.component.scss'
 })
 export class StripeCheckoutSessionSuccessComponent implements OnInit {
   @Input({ required: true }) stripeCheckoutSession!: StripeCheckoutSessionStatus;
   private iconReg = inject(SvgIconRegistryService);
+  private httpService = inject(HttpService);
+  qrCodeResponse$: Observable<string> = EMPTY;
 
   ngOnInit(): void {
     this.iconReg.loadSvg('/svg/rosette-discount-check.svg', 'rosette-discount-check')?.subscribe();
 
     // Display the Stripe Checkout session status
     console.log('Stripe Checkout session status:', this.stripeCheckoutSession);
+
+    // Fetch the QR-Code for the survey
+    const path = `survey/${this.stripeCheckoutSession.surveyId}/viewform`;
+    this.qrCodeResponse$ = this.httpService.post<{ svg: string }>(environment.endpoints.createQRCode, { path }).pipe(
+      map(({ data }) => data.svg),
+      catchError(error => {
+        console.error("Error fetching QR-Code for survey:", error);
+        return EMPTY;
+      })
+    )
   }
 
   ngOnDestroy(): void {
