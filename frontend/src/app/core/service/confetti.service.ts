f75bdfd7c1ee9162733671abb5708fa0d1d2915b
dgschel <goesc_@hotmail.de>
2024-09-17 16:14:48 +0200

    create basic confetti on canvas screen

--- frontend/src/app/core/service/confetti.service.ts
+++ frontend/src/app/core/service/confetti.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import * as confetti from 'canvas-confetti';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ConfettiService {
-
-  constructor() { }
+  celebrate(canvas: HTMLCanvasElement, option: confetti.Options = {}) {
+    return confetti.create(canvas)(option)
+  }
 }
