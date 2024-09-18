ac1d5e7fece7b49cefb0b9dda045488b159d47aa
dgschel <goesc_@hotmail.de>
2024-09-18 13:21:06 +0200

    add bindings

--- backend/src/functions/updateSurveyPaymentStatusHttp.ts
+++ backend/src/functions/updateSurveyPaymentStatusHttp.ts
@@ -1,10 +1,18 @@
-import { app, HttpRequest, HttpResponseInit, input, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, input, InvocationContext, output } from "@azure/functions";
+import { SurveyCosmosDbSchema } from "../schemas/survey";
 
 const surveyInput = input.cosmosDB({
     databaseName: 'SurveyDB',
     containerName: 'Survey',
     connection: 'cosmosDbConnection',
+    id: '{surveyId}', // Use both parameter to fetch a single survey as per documentation
     partitionKey: '{surveyId}',
+});
+
+const surveyOutput = output.cosmosDB({
+    databaseName: 'SurveyDB',
+    containerName: 'Survey',
+    connection: 'cosmosDbConnection',
 });
 
 export async function updateSurveyPaymentStatusHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
@@ -20,5 +28,6 @@ app.http('updateSurveyPaymentStatusHttp', {
     route: 'survey/{surveyId}/payment-status',
     authLevel: 'function',
     extraInputs: [surveyInput],
+    extraOutputs: [surveyOutput],
     handler: updateSurveyPaymentStatusHttp
 });
