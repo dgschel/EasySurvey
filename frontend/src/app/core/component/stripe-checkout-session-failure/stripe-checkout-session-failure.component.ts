15495e1188ae9666ebaef008f2554d53ad7c5e6b
dgschel <goesc_@hotmail.de>
2024-09-18 09:14:13 +0200

    handle case of failure payment

--- frontend/src/app/core/component/stripe-checkout-session-failure/stripe-checkout-session-failure.component.ts
+++ frontend/src/app/core/component/stripe-checkout-session-failure/stripe-checkout-session-failure.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-stripe-checkout-session-failure',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './stripe-checkout-session-failure.component.html',
   styleUrl: './stripe-checkout-session-failure.component.scss'
 })
 export class StripeCheckoutSessionFailureComponent {
-
+  @Input({ required: true }) surveyId: string = '';
 }
