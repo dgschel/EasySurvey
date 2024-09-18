84455e469f59bb4c631e5a40ea967aa917a9046c
dgschel <goesc_@hotmail.de>
2024-09-18 14:10:33 +0200

    create comp to display failure status

---
+++ frontend/src/app/shared/ui/template/modal/survey-update-payment-status-failed/survey-update-payment-status-failed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-update-payment-status-failed',
+  standalone: true,
+  imports: [],
+  templateUrl: './survey-update-payment-status-failed.component.html',
+  styleUrl: './survey-update-payment-status-failed.component.scss'
+})
+export class SurveyUpdatePaymentStatusFailedComponent {
+
+}
