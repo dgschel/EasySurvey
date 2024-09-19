50da59ea4e5d75cd3d9a996936cb802cd20a2205
dgschel <goesc_@hotmail.de>
2024-09-19 10:19:26 +0200

    edit ux-design for failure payment

--- frontend/src/app/core/component/stripe-checkout-session-failure/stripe-checkout-session-failure.component.ts
+++ frontend/src/app/core/component/stripe-checkout-session-failure/stripe-checkout-session-failure.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { DisplayErrorMessageComponent } from '../../../shared/ui/display-error-message/display-error-message.component';
+
+import { SvgIconComponent, SvgIconRegistryService } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-stripe-checkout-session-failure',
   standalone: true,
-  imports: [RouterLink, DisplayErrorMessageComponent],
+  imports: [RouterLink, SvgIconComponent],
   templateUrl: './stripe-checkout-session-failure.component.html',
   styleUrl: './stripe-checkout-session-failure.component.scss'
 })
 export class StripeCheckoutSessionFailureComponent {
   @Input({ required: true }) surveyId: string = '';
+  private iconReg = inject(SvgIconRegistryService);
+
+  ngOnInit(): void {
+    this.iconReg.loadSvg('/svg/rosette-discount-check-off.svg', 'rosette-discount-check-off')?.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.iconReg.unloadSvg('rosette-discount-check-off');
+  }
 }
