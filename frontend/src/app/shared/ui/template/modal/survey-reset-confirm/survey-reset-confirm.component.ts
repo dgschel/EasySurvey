4a4d0b2ec19cfdfb6bc706b90365b72895a34f73
dgschel <goesc_@hotmail.de>
2024-09-25 10:22:48 +0200

    add comp

---
+++ frontend/src/app/shared/ui/template/modal/survey-reset-confirm/survey-reset-confirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-reset-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './survey-reset-confirm.component.html',
+  styleUrl: './survey-reset-confirm.component.scss'
+})
+export class SurveyResetConfirmComponent {
+
+}
