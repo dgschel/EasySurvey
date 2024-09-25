09589bbe4beb75b01317e20742cfac8993ae4f93
dgschel <goesc_@hotmail.de>
2024-09-25 10:41:26 +0200

    emit event on confirm

--- frontend/src/app/shared/ui/template/modal/survey-reset-confirm/survey-reset-confirm.component.ts
+++ frontend/src/app/shared/ui/template/modal/survey-reset-confirm/survey-reset-confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-survey-reset-confirm',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './survey-reset-confirm.component.scss'
 })
 export class SurveyResetConfirmComponent {
-
+  reset = output<void>();
 }
