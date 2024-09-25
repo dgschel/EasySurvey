30b00629e37b01f926ef9752e1944b1bd47d1603
dgschel <goesc_@hotmail.de>
2024-09-25 07:38:08 +0200

    add comp

---
+++ frontend/src/app/core/component/survey-paid-form/survey-paid-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-paid-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './survey-paid-form.component.html',
+  styleUrl: './survey-paid-form.component.scss'
+})
+export class SurveyPaidFormComponent {
+
+}
