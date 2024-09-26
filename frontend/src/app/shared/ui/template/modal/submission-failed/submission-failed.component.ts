706df10b10ea5af026c5a86e8c274a15d8b3754a
dgschel <goesc_@hotmail.de>
2024-09-26 10:09:50 +0200

    add comp

---
+++ frontend/src/app/shared/ui/template/modal/submission-failed/submission-failed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission-failed',
+  standalone: true,
+  imports: [],
+  templateUrl: './submission-failed.component.html',
+  styleUrl: './submission-failed.component.scss'
+})
+export class SubmissionFailedComponent {
+
+}
