69473eeb67c48ef7f3cd83cb3e4625f951cb9921
dgschel <goesc_@hotmail.de>
2024-09-26 06:59:58 +0200

    update template

--- frontend/src/app/shared/ui/template/modal/submission-succesfully-saved/submission-succesfully-saved.component.ts
+++ frontend/src/app/shared/ui/template/modal/submission-succesfully-saved/submission-succesfully-saved.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { DisplayQrCodeComponent } from "../../../display-qr-code/display-qr-code.component";
 
 @Component({
   selector: 'app-submission-succesfully-saved',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, DisplayQrCodeComponent],
   templateUrl: './submission-succesfully-saved.component.html',
   styleUrl: './submission-succesfully-saved.component.scss'
 })
 export class SubmissionSuccesfullySavedComponent {
-
+  @Input({ required: true }) surveyId!: string;
+  @Input({ required: true }) svg!: string;
 }
