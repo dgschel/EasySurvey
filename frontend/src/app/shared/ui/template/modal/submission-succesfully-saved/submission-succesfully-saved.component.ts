878454f4ca4fbed54cd7ae208f04c650bb4ea956
dgschel <goesc_@hotmail.de>
2024-09-26 06:48:31 +0200

    create comp

---
+++ frontend/src/app/shared/ui/template/modal/submission-succesfully-saved/submission-succesfully-saved.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission-succesfully-saved',
+  standalone: true,
+  imports: [],
+  templateUrl: './submission-succesfully-saved.component.html',
+  styleUrl: './submission-succesfully-saved.component.scss'
+})
+export class SubmissionSuccesfullySavedComponent {
+
+}
