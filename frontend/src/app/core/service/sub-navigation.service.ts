b87ef4e4f248f902b59e3b0ba1827dd4449a4e68
dgschel <goesc_@hotmail.de>
2024-09-26 14:17:55 +0200

    create subject for route navigation

--- frontend/src/app/core/service/sub-navigation.service.ts
+++ frontend/src/app/core/service/sub-navigation.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+interface SubNavigationConfig {
+  show: boolean;
+  tabs: Array<{ label: string, route: string }>;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubNavigationService {
+  private subNavConfigSubject = new BehaviorSubject<SubNavigationConfig>({
+    show: false,
+    tabs: []
+  });
 
-  constructor() { }
+  subNavConfig$ = this.subNavConfigSubject.asObservable();
+
+  updateSubNavConfig(config: SubNavigationConfig) {
+    this.subNavConfigSubject.next(config);
+  }
+
+  clearSubNavConfig() {
+    this.subNavConfigSubject.next({ show: false, tabs: [] });
+  }
 }
