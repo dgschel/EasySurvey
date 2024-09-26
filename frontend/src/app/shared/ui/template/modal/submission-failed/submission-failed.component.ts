a2e6371b2f794d1dac0a2b1d9dd8398f5fa96b43
dgschel <goesc_@hotmail.de>
2024-09-26 10:32:10 +0200

    updated modal text and receive surveyId

--- frontend/src/app/shared/ui/template/modal/submission-failed/submission-failed.component.ts
+++ frontend/src/app/shared/ui/template/modal/submission-failed/submission-failed.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CopyToClipboardComponent } from "../../../../feature/copy-to-clipboard/copy-to-clipboard.component";
 
 @Component({
   selector: 'app-submission-failed',
   standalone: true,
-  imports: [],
+  imports: [CopyToClipboardComponent],
   templateUrl: './submission-failed.component.html',
   styleUrl: './submission-failed.component.scss'
 })
 export class SubmissionFailedComponent {
-
+  @Input({ required: true }) surveyId!: string;
 }
