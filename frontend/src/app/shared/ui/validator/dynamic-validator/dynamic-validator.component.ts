1feffb387bbf79e6f4ef6600f586cbb53ff6b183
dgschel <goesc_@hotmail.de>
2024-09-28 09:34:16 +0200

    use ngcomponentoutlet to dynamically load comp

--- frontend/src/app/shared/ui/validator/dynamic-validator/dynamic-validator.component.ts
+++ frontend/src/app/shared/ui/validator/dynamic-validator/dynamic-validator.component.ts
@@ -1,14 +1,20 @@
+import { NgComponentOutlet } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import { SurveyValidatorType, ValidatorComponentType } from '../../../../util/type/survey-type';
+import { validatorComponentTypeMap } from '../../../form-validator/validators';
+
 @Component({
   selector: 'app-dynamic-validator',
   standalone: true,
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './dynamic-validator.component.html',
   styleUrl: './dynamic-validator.component.scss'
 })
 export class DynamicValidatorComponent {
-  @Input() validators: string[] = [];
   @Input() validators: SurveyValidatorType[] = [];
+
+  getValidatorComponent(validator: SurveyValidatorType): ValidatorComponentType {
+    return validatorComponentTypeMap[validator];
+  }
 }
