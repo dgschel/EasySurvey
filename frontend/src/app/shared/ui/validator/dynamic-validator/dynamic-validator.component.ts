7b2ced75da821c8791d3a13bc7c018d81a145f32
dgschel <goesc_@hotmail.de>
2024-09-29 13:26:25 +0200

    pass down validator state

--- frontend/src/app/shared/ui/validator/dynamic-validator/dynamic-validator.component.ts
+++ frontend/src/app/shared/ui/validator/dynamic-validator/dynamic-validator.component.ts
@@ -1,7 +1,7 @@
 import { NgComponentOutlet } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { SurveyValidatorType, ValidatorComponentType, ValidatorValueChange } from '../../../../util/type/survey-type';
+import { SurveyValidatorType, ValidatorComponentType, ValidatorConfig, ValidatorValueChange } from '../../../../util/type/survey-type';
 import { validatorComponentTypeMap } from '../../../form-validator/validators';
 
 @Component({
@@ -13,11 +13,22 @@ import { validatorComponentTypeMap } from '../../../form-validator/validators';
 })
 export class DynamicValidatorComponent {
   @Input() validators: SurveyValidatorType[] = [];
+  @Input() state: Partial<ValidatorConfig> = {};
   @Output() validatorValueChange = new EventEmitter<ValidatorValueChange>();
 
   // Callback function pass down to the child component using the @Input() decorator
   onValidatorValueChange = (change: ValidatorValueChange) => {
     this.validatorValueChange.emit({ validatorType: change.validatorType, value: change.value });
+  }
+
+  // Get the input object for the validator component
+  getInputs(validator: SurveyValidatorType): Record<string, unknown> {
+    // Interface ValidatorComponentInput<T>
+    // We are returning an object to satisfy the interface of ngComponentOutlet
+    return {
+      onValidatorValueChange: this.onValidatorValueChange,
+      value: this.state[validator]?.value
+    }
   }
 
   getValidatorComponent(validator: SurveyValidatorType): ValidatorComponentType {
