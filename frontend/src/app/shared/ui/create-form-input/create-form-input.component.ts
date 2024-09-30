129d62271cb55c6061181e330d4ffe3c13bf962c
dgschel <goesc_@hotmail.de>
2024-09-30 12:43:41 +0200

    remove padding

--- frontend/src/app/shared/ui/create-form-input/create-form-input.component.ts
+++ frontend/src/app/shared/ui/create-form-input/create-form-input.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-create-form-input',
   standalone: true,
   imports: [],
-  template: '<div class="border-b border-dashed leading-7 px-4 w-40 text-gray-500">Kurzantwort</div>'
+  template: '<div class="border-b border-dashed leading-7 w-40 text-gray-500">Kurzantwort</div>'
 })
 export class CreateFormInputComponent { }
