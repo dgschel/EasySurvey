4bb297c85c868c0b4b3db5dd6cc9854375cc47b2
David Göschel <goesc_@hotmail.de>
2024-10-18 07:00:11 +0200

    add basic accordion functionality

--- frontend/src/app/shared/ui/display-accordion/display-accordion.component.ts
+++ frontend/src/app/shared/ui/display-accordion/display-accordion.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-display-accordion',
   standalone: true,
   imports: [],
   templateUrl: './display-accordion.component.html',
-  styleUrl: './display-accordion.component.scss'
+  styleUrl: './display-accordion.component.scss',
 })
 export class DisplayAccordionComponent {
-
+  accordions = input<{ title: string; content: string }[]>([]);
 }
