b6d6b3a7ab2f5fe206ec6a622f6868316516ba1e
David Göschel <goesc_@hotmail.de>
2024-10-18 19:33:18 +0200

    use lazy loaded component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,61 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ViewSurveyFormComponent } from './view-survey-form/view-survey-form.component';
-import { StoreFormComponent } from './store-form/store-form.component';
-import { StatisticComponent } from './statistic/statistic.component';
-import { StripeCheckoutSessionComponent } from './core/component/stripe-checkout-session/stripe-checkout-session.component';
-import { SurveyCheckoutComponent } from './survey-checkout/survey-checkout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home/:template', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'store', component: StoreFormComponent },
-  { path: 'survey/:id/statistic', component: StatisticComponent },
-  { path: 'survey/:id/viewform', component: ViewSurveyFormComponent },
-  { path: 'survey/:id/checkout', component: SurveyCheckoutComponent },
-  { path: 'checkout/return', component: StripeCheckoutSessionComponent },
-  { path: '**', redirectTo: '/home' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: ':template',
+        loadComponent: () =>
+          import('./home/home.component').then((m) => m.HomeComponent),
+      },
+    ],
+  },
+  {
+    path: 'store',
+    loadComponent: () =>
+      import('./store-form/store-form.component').then(
+        (m) => m.StoreFormComponent,
+      ),
+  },
+  {
+    path: 'survey',
+    children: [
+      {
+        path: ':id/statistic',
+        loadComponent: () =>
+          import('./statistic/statistic.component').then(
+            (m) => m.StatisticComponent,
+          ),
+      },
+      {
+        path: ':id/viewform',
+        loadComponent: () =>
+          import('./view-survey-form/view-survey-form.component').then(
+            (m) => m.ViewSurveyFormComponent,
+          ),
+      },
+      {
+        path: ':id/checkout',
+        loadComponent: () =>
+          import('./survey-checkout/survey-checkout.component').then(
+            (m) => m.SurveyCheckoutComponent,
+          ),
+      },
+    ],
+  },
+  {
+    path: 'checkout/return',
+    loadComponent: () =>
+      import(
+        './core/component/stripe-checkout-session/stripe-checkout-session.component'
+      ).then((m) => m.StripeCheckoutSessionComponent),
+  },
+  { path: '**', redirectTo: 'home' },
 ];
