d7289cd8a7cc63eb05e2180ff9c1d7380348d10b
David Göschel <goesc_@hotmail.de>
2024-10-18 18:50:04 +0200

    formatting and consume static data

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,23 +4,34 @@ import { ActivatedRoute } from '@angular/router';
 import { SurveyTemplateManager } from '../core/model/survey-template-manager';
 import { SurveyFormComponent } from '../shared/feature/survey-form/survey-form.component';
 import { SurveyModel } from '../util/type/survey-type';
+import {
+  AccordionItem,
+  DisplayAccordionComponent,
+} from '../shared/ui/display-accordion/display-accordion.component';
+import { FaqService } from '../core/service/faq.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SurveyFormComponent],
+  imports: [SurveyFormComponent, DisplayAccordionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   surveyManager = inject(SurveyTemplateManager);
   route = inject(ActivatedRoute);
+  faqService = inject(FaqService);
 
   models: SurveyModel[] = [];
+  faqs: AccordionItem[] = [];
 
   ngOnInit() {
+    this.faqService.getFaqData().subscribe((data) => (this.faqs = data));
+    
     const surveyTemplate = this.route.snapshot.paramMap.get('template');
-    const survey = this.surveyManager.surveyTemplateModels.find(model => model.name.toLowerCase() === surveyTemplate);
+    const survey = this.surveyManager.surveyTemplateModels.find(
+      (model) => model.name.toLowerCase() === surveyTemplate,
+    );
 
     if (survey) {
       this.models = survey.models;
