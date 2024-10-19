155f647a93fad0112d1b54aeaf19b45c96c06971
David Göschel <goesc_@hotmail.de>
2024-10-19 10:33:38 +0200

    Revert "format doc"
    This reverts commit 6d08d347f8c0755749575179725dbce7e7b20c40.

--- frontend/src/app/shared/ui/template/modal/preview-survey/preview-survey.component.ts
+++ frontend/src/app/shared/ui/template/modal/preview-survey/preview-survey.component.ts
@@ -13,7 +13,6 @@ import { SurveyTemplateManager } from '../../../../../core/model/survey-template
   styleUrl: './preview-survey.component.scss'
 })
 export class PreviewSurveyComponent implements AfterViewInit {
-  // TODO: This component is currently used in survey store to preview only template survey
   surveyTemplateManager = inject(SurveyTemplateManager);
   @Input() surveyName: SurveyName = 'Standard';
   @ViewChild('container', { static: true, read: TemplateRef }) containerTemplate!: TemplateRef<any>;
