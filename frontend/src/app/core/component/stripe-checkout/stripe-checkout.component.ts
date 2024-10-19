2e166890e34116bc83c29a0c9e03a3874d5304b8
David Göschel <goesc_@hotmail.de>
2024-10-19 11:00:07 +0200

    format comp

--- frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
+++ frontend/src/app/core/component/stripe-checkout/stripe-checkout.component.ts
@@ -7,16 +7,23 @@ import { SvgIconComponent } from 'angular-svg-icon';
 
 import { StripeCheckoutService } from '../../service/stripe-checkout.service';
 import { DisplayErrorMessageComponent } from '../../../shared/ui/display-error-message/display-error-message.component';
-import { BasicCardComponent } from "../../../shared/ui/basic-card/basic-card.component";
-import { StripeEmbeddedCheckoutFormComponent } from "../stripe-embedded-checkout-form/stripe-embedded-checkout-form.component";
-import { LoadingComponent } from "../../../shared/ui/loading/loading.component";
+import { BasicCardComponent } from '../../../shared/ui/basic-card/basic-card.component';
+import { StripeEmbeddedCheckoutFormComponent } from '../stripe-embedded-checkout-form/stripe-embedded-checkout-form.component';
+import { LoadingComponent } from '../../../shared/ui/loading/loading.component';
 
 @Component({
   selector: 'app-stripe-checkout',
   standalone: true,
-  imports: [AsyncPipe, DisplayErrorMessageComponent, SvgIconComponent, BasicCardComponent, StripeEmbeddedCheckoutFormComponent, LoadingComponent],
+  imports: [
+    AsyncPipe,
+    DisplayErrorMessageComponent,
+    SvgIconComponent,
+    BasicCardComponent,
+    StripeEmbeddedCheckoutFormComponent,
+    LoadingComponent,
+  ],
   templateUrl: './stripe-checkout.component.html',
-  styleUrl: './stripe-checkout.component.scss'
+  styleUrl: './stripe-checkout.component.scss',
 })
 export class StripeCheckoutComponent implements OnInit {
   @Input('surveyId') surveyId: string = '';
@@ -27,50 +34,51 @@ export class StripeCheckoutComponent implements OnInit {
   errorMessage: string = '';
 
   ngOnInit() {
-
     // Fetch client secret for Stripe Checkout
     const fetchClientSecretObservable$ = this.stripeService.fetchClientSecret(this.surveyId).pipe(
-      catchError(error => {
-        this.errorMessage = "Fehler! Beim Abrufen des Client-Secrets ist ein Fehler aufgetreten";
-        console.error("Error fetching client secret:", error);
+      catchError((error) => {
+        this.errorMessage = 'Fehler! Beim Abrufen des Client-Secrets ist ein Fehler aufgetreten';
+        console.error('Error fetching client secret:', error);
         return EMPTY; // Stop further processing if client secret cannot be fetched
-      })
+      }),
     );
 
     // Initialize Stripe Checkout
     const initializedStripeCheckout$ = fetchClientSecretObservable$.pipe(
       switchMap(({ data }) => {
         if (!data || !data.clientSecret) {
-          this.errorMessage = "Fehler! Ungültiges Client Secret für Stripe Checkout";
-          console.error("Invalid client secret for Stripe Checkout");
+          this.errorMessage = 'Fehler! Ungültiges Client Secret für Stripe Checkout';
+          console.error('Invalid client secret for Stripe Checkout');
           return EMPTY; // Ensure client secret is valid before proceeding
         }
         return from(this.stripeService.initEmbeddedCheckout(data.clientSecret)).pipe(
-          catchError(error => {
-            this.errorMessage = "Fehler! Beim Initialisieren von Stripe Checkout ist ein Fehler aufgetreten";
-            console.error("Error initializing Stripe Checkout:", error);
+          catchError((error) => {
+            this.errorMessage = 'Fehler! Beim Initialisieren von Stripe Checkout ist ein Fehler aufgetreten';
+            console.error('Error initializing Stripe Checkout:', error);
             return EMPTY; // Stop further processing if Stripe Checkout initialization fails
-          })
+          }),
         );
-      })
+      }),
     );
 
     // Subscribe to the initialized Stripe Checkout observable
     this.checkout$ = initializedStripeCheckout$.pipe(
-      switchMap(checkout => {
+      switchMap((checkout) => {
         if (!checkout) {
-          this.errorMessage = "Fehler! Stripe Checkout konnte nicht initialisiert werden. Bitte versuchen Sie es später erneut";
-          console.error("Error: Stripe checkout could not be initialized");
+          this.errorMessage =
+            'Fehler! Stripe Checkout konnte nicht initialisiert werden. Bitte versuchen Sie es später erneut';
+          console.error('Error: Stripe checkout could not be initialized');
           return EMPTY; // Stop further processing if Stripe Checkout initialization fails
         }
 
         return of(checkout); // Return the initialized Stripe Checkout
       }),
-      catchError(error => {
-        this.errorMessage = "Fehler! Beim Checkout-Prozess ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut";
-        console.error("Error in the checkout process:", error);
+      catchError((error) => {
+        this.errorMessage =
+          'Fehler! Beim Checkout-Prozess ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut';
+        console.error('Error in the checkout process:', error);
         return EMPTY; // Stop further processing if checkout process fails
-      })
+      }),
     );
   }
 }
