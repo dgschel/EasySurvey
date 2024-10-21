f18df9c22ce2ef9494ca5c83a78220be294e6393
David Göschel <goesc_@hotmail.de>
2024-10-21 12:40:47 +0200

    add binding to read all surveys

--- backend/src/functions/manageSurveyLifecycle.ts
+++ backend/src/functions/manageSurveyLifecycle.ts
@@ -1,4 +1,12 @@
-import { app, InvocationContext, Timer } from "@azure/functions";
+import { app, input, InvocationContext, Timer } from "@azure/functions";
+import { SurveyModel } from "../models/survey";
+import { SurveyCosmosDbSchema } from "../schemas/survey";
+
+const surveyInput = input.cosmosDB({
+    databaseName: 'SurveyDB',
+    containerName: 'Survey',
+    connection: 'cosmosDbConnection',
+});
 
 export async function manageSurveyLifecycle(myTimer: Timer, context: InvocationContext): Promise<void> {
     const timeStamp = new Date().toISOString();
@@ -7,5 +15,6 @@ export async function manageSurveyLifecycle(myTimer: Timer, context: InvocationC
 
 app.timer('ManageSurveyLifecycle', {
     schedule: '0 0 2 * * *',
-    handler: manageSurveyLifecycle
+    handler: manageSurveyLifecycle,
+    extraInputs: [surveyInput]
 });
