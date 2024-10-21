eb1f26498e011a41fea8dfc3549296a09c054f76
David Göschel <goesc_@hotmail.de>
2024-10-21 22:27:13 +0200

    completely rewrite of fetching expired surveys WIP

--- backend/src/functions/manageSurveyLifecycle.ts
+++ backend/src/functions/manageSurveyLifecycle.ts
@@ -1,52 +1,47 @@
-import { app, input, InvocationContext, Timer } from "@azure/functions";
-import { SurveyCosmosDbSchema } from "../schemas/survey";
+import { app, InvocationContext, Timer } from "@azure/functions";
+import { CosmosClient, FeedResponse } from "@azure/cosmos";
+import { DefaultAzureCredential, TokenCredential } from "@azure/identity";
 
-const SEVEN_DAYS = 7 * 24 * 60 * 60 * 1000;
+import { buildExpiredDocumentsQuery } from "../util/query";
+import { SurveyCosmosDb } from "../models/survey";
+import { cosmosDB } from "@azure/functions/types/app";
 
-const surveyInput = input.cosmosDB({
-    databaseName: 'SurveyDB',
-    containerName: 'Survey',
-    connection: 'cosmosDbConnection',
-});
+export async function manageSurveyLifecycle(
+  myTimer: Timer,
+  context: InvocationContext
+): Promise<void> {
+  context.log("Timer function processed request at", new Date().toISOString());
 
-export async function manageSurveyLifecycle(myTimer: Timer, context: InvocationContext): Promise<void> {
-    const timeStamp = new Date().toISOString();
-    context.log('Timer function processed request at', timeStamp);
+  const secondsSinceEpoch = Math.floor(Date.now() / 1000);
+  const expiredDocumentsQuery = buildExpiredDocumentsQuery(secondsSinceEpoch);
 
-    try {
-        // Get the survey with the surveyId
-        const survey = context.extraInputs.get(surveyInput);
+  try {
+    const aadCredentials: TokenCredential = new DefaultAzureCredential();
+    const endpoint = process.env.cosmosDbEndpoint;
+    const client = new CosmosClient({ aadCredentials, endpoint });
 
-        // Try to parse the survey data
-        const parsedSurveys = SurveyCosmosDbSchema.pick({ status: true, _ts: true }).array().safeParse(survey);
+    const container = client
+      .database(process.env.COSMOS_DB_NAME)
+      .container(process.env.COSMOS_DB_CONTAINER);
 
-        // If the survey data is not valid, throw an error
-        if (!parsedSurveys.success) {
-            context.log(`Validation errors:`, parsedSurveys.error.errors);
-            throw new Error("Survey data is invalid");
-        }
+    context.log(
+      `Connected to the database ${process.env.COSMOS_DB_NAME} and container ${process.env.COSMOS_DB_CONTAINER}`
+    );
 
-        context.log("Survey data", parsedSurveys.data);
+    context.log("Querying expired documents with query", expiredDocumentsQuery);
 
-        const isOlder = isDocumentExpired(parsedSurveys.data[parsedSurveys.data.length - 1]._ts);
-        // Prüfe und lösche nicht bezahlte Formulare, die älter als 7 Tage sind
-    } catch (error) {
-        context.log("An error occurred while processing the survey data", error);
-    }
+    const expiredSurveys: FeedResponse<SurveyCosmosDb> = await container.items
+      .query(expiredDocumentsQuery)
+      .fetchAll();
+
+    context.log("Expired documents", expiredSurveys.resources);
+
+  } catch (error) {
+    context.log("An error occurred while connecting to the database", error);
+  }
 }
 
-function isDocumentExpired(ts: number): boolean {
-    // convert the timestamp to a date
-    const documentDate = new Date(ts * 1000);
-
-    // subtract from the current date to determine if the document is older than x days
-    const sevenDaysAgo = new Date(Date.now() - SEVEN_DAYS);
-
-    return documentDate < sevenDaysAgo;
-}
-
-app.timer('ManageSurveyLifecycle', {
-    schedule: '0 0 2 * * *',
-    handler: manageSurveyLifecycle,
-    extraInputs: [surveyInput]
+app.timer("manageSurveyLifecycle", {
+  schedule: "*/20 * * * * *",
+  handler: manageSurveyLifecycle,
 });
