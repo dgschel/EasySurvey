20f60085e032370642d88d9372578e530249f8e5
David Göschel <goesc_@hotmail.de>
2024-10-21 12:42:04 +0200

    get all surveys and safeparse the result

--- backend/src/functions/manageSurveyLifecycle.ts
+++ backend/src/functions/manageSurveyLifecycle.ts
@@ -11,6 +11,26 @@ const surveyInput = input.cosmosDB({
 export async function manageSurveyLifecycle(myTimer: Timer, context: InvocationContext): Promise<void> {
     const timeStamp = new Date().toISOString();
     context.log('Timer function processed request at', timeStamp);
+
+    try {
+        // Get the survey with the surveyId
+        const survey = context.extraInputs.get(surveyInput);
+
+        // Try to parse the survey data
+        const parsedSurveys = SurveyCosmosDbSchema.pick({ status: true }).array().safeParse(survey);
+
+        // If the survey data is not valid, throw an error
+        if (!parsedSurveys.success) {
+            context.log(`Validation errors:`, parsedSurveys.error.errors);
+            throw new Error("Survey data is invalid");
+        }
+
+        context.log("Survey data", parsedSurveys.data);
+        
+        // Prüfe und lösche nicht bezahlte Formulare, die älter als 7 Tage sind
+    } catch (error) {
+        context.log("An error occurred while processing the survey data", error);
+    }
 }
 
 app.timer('ManageSurveyLifecycle', {
