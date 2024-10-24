1335d09a6c5f8e664505384767ff3896f9ba54eb
David Göschel <goesc_@hotmail.de>
2024-10-24 06:54:35 +0200

    add stripe consent comp

---
+++ frontend/src/app/features/survey/components/stripe-consent/stripe-consent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-consent',
+  standalone: true,
+  imports: [],
+  templateUrl: './stripe-consent.component.html',
+  styleUrl: './stripe-consent.component.scss'
+})
+export class StripeConsentComponent {
+
+}
